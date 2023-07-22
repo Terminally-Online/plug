67102fe4c7298b0652ea9db0f8a2f6f8f906f0ad
⚪ CHANCE <chance+sft@utc24.io>
2023-07-22 14:26:26 -0500

    feat: docs

---
+++ src/engine/consumer.ts
@@ -0,0 +1,5 @@
+export class Consumer {
+    constructor() {
+        console.log('Consumer')
+    }
+}
---
+++ src/engine/deploy.ts
@@ -0,0 +1,6 @@
+// import { DeployIntent } from '../types/intents'
+
+// To test this:
+// - Build a deployment intent for a contract.
+// - Sign intent.
+// - Submit intent.
