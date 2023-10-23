fa0b0db0a23462b833064c65c42964ae11518ec0
nftchance <chance@utc24.io>
2023-10-23 16:24:35 -0500

    feat: remove playground

--- packages/playground/emporium.config.ts
+++
@@ -1,8 +0,0 @@
-import { config } from '@nftchance/emporium-types'
-
-// * With an empty config, default values will be used for all the fields
-//   when generating the Solidity smart contract. For basic usage, this is
-//   probably what you want. If have additional events or need to customize
-//   the static declarations of the contract, you can pass in a partial
-//   config object to override the default values.
-export default config({ out: './dist/contracts/' })
