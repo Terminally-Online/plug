d261074c65257bbb1a7727ab34df900c4c5bb1b0
nftchance <chance@utc24.io>
2023-10-28 00:38:21 -0500

    fix: operational tests

--- src/intent.ts
+++ src/intent.ts
@@ -11,31 +11,6 @@ import {
 
 import { TypedDataToKeysWithSignedPair } from './lib/types'
 
-const DEBUG_TYPES = {
-	Mail: [
-		{ name: 'from', type: 'Person' },
-		{ name: 'to', type: 'Person' },
-		{ name: 'contents', type: 'string' }
-	],
-	Person: [
-		{ name: 'name', type: 'string' },
-		{ name: 'wallet', type: 'address' }
-	],
-	Email: [
-		{ name: 'from', type: 'Person' },
-		{ name: 'to', type: 'Person[]' },
-		{ name: 'mail', type: 'SignedMail[]' }
-	],
-	SignedMail: [
-		{ name: 'mail', type: 'Mail' },
-		{ name: 'signature', type: 'bytes' }
-	],
-	SignedEmail: [
-		{ name: 'email', type: 'Email' },
-		{ name: 'signature', type: 'bytes' }
-	]
-} as const
-
 export type IntentType<T> = Exclude<
 	{
 		[K in keyof T]: `Signed${Capitalize<string & K>}` extends keyof T
@@ -77,27 +52,6 @@ export class Intent<
 
 		this.client = client
 
-		if (!this.client.account)
-			throw new Error('Client account not initialized')
-
-		await this.client.signTypedData({
-			account: this.client.account.address,
-			domain: this.domain,
-			types: DEBUG_TYPES,
-			primaryType: 'Mail',
-			message: {
-				from: { name: 'Bob', wallet: '0x0' },
-				to: { name: 'Alice', wallet: '0x0' },
-				contents: 'Hello, world!'
-			}
-		})
-
-		if (!this.message) return
-
-		if (!this.domain) throw new Error('Domain not initialized')
-		if (!this.types) throw new Error('Types not initialized')
-		if (!this.message) throw new Error('Message not initialized')
-		if (!this.primaryType) throw new Error('Primary type not initialized')
 		if (!this.client.account)
 			throw new Error('Client account not initialized')
 
