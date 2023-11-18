3d291cfcccb68f4421ff7d252b0c0a17ebbbe636
nftchance <chance@utc24.io>
2023-11-17 21:15:41 -0600

    feat: wallet based authentication

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,67 +1,56 @@
 import NextAuth from 'next-auth';
 import { AppProviders } from 'next-auth/providers/index';
 import CredentialsProvider from 'next-auth/providers/credentials';
-import GithubProvider from 'next-auth/providers/github';
+import { getCsrfToken } from 'next-auth/react';
+import { SiweMessage } from 'siwe';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
-// * This generates /auth/signin, /auth/signout, /auth/callback/, 
+const providers = [CredentialsProvider({
+    name: "Ethereum",
+    credentials: {
+        message: { 
+            label: "Message", 
+            type: "text", 
+            placeholder: "0x0" 
+        },
+        signature: { 
+            label: "Signature",
+            type: "text",
+            placeholder: "0x0"
+        }
+    },
+    async authorize(credentials, req) { 
+        try { 
+            const siwe = new SiweMessage(JSON.parse(credentials?.message || "{}"));
+            const nextAuthUrl = new URL(process.env.NEXTAUTH_URL || "http://localhost:3000/api/auth");
+            const result = await siwe.verify({ 
+                signature: credentials?.signature || '',
+                domain: nextAuthUrl.host,
+                nonce: await getCsrfToken({ req: { headers: req.headers } })
+            })
 
-let useMockProvider = process.env.NODE_ENV === 'test';
-
-const { GITHUB_CLIENT_ID, GITHUB_SECRET, NODE_ENV, APP_ENV } = process.env;
-if (
-    (NODE_ENV !== 'production' || APP_ENV === 'test') &&
-        (!GITHUB_CLIENT_ID || !GITHUB_SECRET)
-) {
-    console.log('⚠️ Using mocked GitHub auth correct credentials were not added');
-    useMockProvider = true;
-}
-
-const providers: AppProviders = [];
-if (useMockProvider) {
-    providers.push(
-        CredentialsProvider({
-            id: 'github',
-            name: 'Mocked GitHub',
-            async authorize(credentials) {
-                if (credentials) {
-                    const name = credentials.name;
-                    return {
-                        id: name,
-                        name: name,
-                        email: name,
-                    };
-                }
-                return null;
-            },
-            credentials: {
-                name: { type: 'test' },
-            },
-        }),
-    );
-} else {
-    if (!GITHUB_CLIENT_ID || !GITHUB_SECRET) {
-        throw new Error('GITHUB_CLIENT_ID and GITHUB_SECRET must be set');
+            if(result.success) { return { id: siwe.address } }
+            return null
+        } catch (e) { 
+            return null
+        }
     }
-    providers.push(
-        GithubProvider({
-            clientId: GITHUB_CLIENT_ID,
-            clientSecret: GITHUB_SECRET,
-            profile(profile) {
-                return {
-                    id: profile.id,
-                    name: profile.login,
-                    email: profile.email,
-                    image: profile.avatar_url,
-                } as any;
-            },
-        }),
-    );
+}) ]
+
+const callbacks = { 
+    async session({ session, token }: { session: any, token: any }) {
+        session.address = token.sub
+        session.user.name = token.sub
+        session.user.image = `https://avatar.vercel.sh/${pin.value}.png`
+        return session
+    }
 }
 
-const handler = NextAuth({
-    secret: 'testasafd',
-    // Configure one or more authentication providers
+const handler = NextAuth({ 
     providers,
-});
+    session: { strategy: "jwt" },
+    secret: process.env.NEXTAUTH_SECRET || "secret",
+    callbacks
+})
 
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST }
