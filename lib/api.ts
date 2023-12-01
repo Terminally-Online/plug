7e6e21de04cc4b5de03cc6aaee45571079708796
nftchance <chance@utc24.io>
2023-12-01 13:24:07 -0600

    feat: api cleanup

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,7 @@
 import { NextPageContext } from 'next'
 
+import getConfig from 'next/config'
+
 import superjson from 'superjson'
 
 import { createWSClient, httpBatchLink, loggerLink, wsLink } from '@trpc/client'
@@ -8,37 +10,7 @@ import { type inferRouterInputs, type inferRouterOutputs } from '@trpc/server'
 
 import { type AppRouter } from '@/server/api/root'
 
-const getBaseUrl = () => {
-	if (typeof window !== 'undefined') return '' // browser should use relative url
-
-	if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}` // SSR should use vercel url
-
-	return `http://localhost:${process.env.PORT ?? 3000}` // dev SSR should use localhost
-}
-
-function getEndingLink(ctx: NextPageContext | undefined) {
-	if (typeof window === 'undefined') {
-		return httpBatchLink({
-			url: `${getBaseUrl()}/api/trpc`,
-			headers() {
-				if (!ctx?.req?.headers) {
-					return {}
-				}
-				// on ssr, forward client's headers to the server
-				return {
-					...ctx.req.headers,
-					'x-ssr': '1'
-				}
-			}
-		})
-	}
-	const client = createWSClient({
-		url: `ws://localhost:3001`
-	})
-	return wsLink<AppRouter>({
-		client
-	})
-}
+import { createLinks } from './links'
 
 /** A set of type-safe react-query hooks for your tRPC API. */
 export const api = createTRPCNext<AppRouter>({
@@ -56,15 +28,7 @@ export const api = createTRPCNext<AppRouter>({
 			 *
 			 * @see https://trpc.io/docs/links
 			 */
-			links: [
-				loggerLink({
-					enabled: opts =>
-						process.env.NODE_ENV === 'development' ||
-						(opts.direction === 'down' &&
-							opts.result instanceof Error)
-				}),
-				getEndingLink(ctx)
-			]
+			links: createLinks(ctx)
 		}
 	},
 	/**
---
+++ lib/links.ts
@@ -0,0 +1,48 @@
+import { NextPageContext } from 'next'
+
+import { createWSClient, httpBatchLink, loggerLink, wsLink } from '@trpc/client'
+
+import { type AppRouter } from '@/server/api/root'
+
+const getBaseUrl = () => {
+	// browser should use relative url
+	if (typeof window !== 'undefined') return ''
+
+	// SSR should use vercel url
+	if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`
+
+	// dev SSR should use localhost
+	return `http://localhost:${process.env.PORT ?? 3000}`
+}
+
+function getEndingLink(ctx: NextPageContext | undefined) {
+	if (typeof window === 'undefined') {
+		return httpBatchLink({
+			url: `${process.env.APP_URL || getBaseUrl()}/api/trpc`,
+			headers() {
+				if (!ctx?.req?.headers) {
+					return {}
+				}
+				return {
+					...ctx.req.headers,
+					'x-ssr': '1'
+				}
+			}
+		})
+	}
+	const client = createWSClient({
+		url: process.env.WS_URL || `ws://localhost:3001`
+	})
+	return wsLink<AppRouter>({
+		client
+	})
+}
+
+export const createLinks = (ctx: NextPageContext | undefined) => [
+	loggerLink({
+		enabled: opts =>
+			process.env.NODE_ENV === 'development' ||
+			(opts.direction === 'down' && opts.result instanceof Error)
+	}),
+	getEndingLink(ctx)
+]
