f9f58c263ec3f35a97b075e333efe33fd437b339
nftchance <chance@utc24.io>
2023-12-05 14:49:30 -0600

    fix: use alias path

--- components/viewport/viewport.tsx
+++ components/viewport/viewport.tsx
@@ -13,7 +13,7 @@ import useSize from "@react-hook/size"
 import useRenderLoop from "@/lib/hooks/useRenderLoop"
 import CanvasStore from "@/lib/store"
 
-import Canvas from "../canvas/canvas"
+import Canvas from "@/components/canvas/canvas"
 
 export type ViewportProps = {
 	id: string
