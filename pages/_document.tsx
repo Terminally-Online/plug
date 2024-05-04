feec85e596cd42bd5628c8d3af5b0219f63efe9b
nftchance <chance@utc24.io>
2024-05-03 23:00:23 -0500

    chore: opengraph and favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,56 @@ import { Head, Html, Main, NextScript } from "next/document"
 export default function Document() {
 	return (
 		<Html lang="en">
-			<Head />
+			<Head>
+				{/* Favicon */}
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/apple-touch-icon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/favicon-16x16.png"
+				/>
+				<link rel="manifest" href="/site.webmanifest" />
+				<meta name="msapplication-TileColor" content="#da532c" />
+				<meta name="theme-color" content="#ffffff" />
+
+				{/* Open Graph */}
+				<meta property="og:title" content="Plug" />
+				<meta property="og:type" content="website" />
+				<meta property="og:url" content="https://onplug.io" />
+				<meta
+					property="og:image"
+					content="https://onplug.io/opengraph.png"
+				/>
+				<meta
+					property="og:description"
+					content="Plug brings trustless automation and scheduling to the entirety of the Ethereum ecosystem with an intent powered framework. The onchain outcomes you want can finally be achieved without being glued to your device."
+				/>
+
+				{/* Twitter */}
+				<meta name="twitter:card" content="summary_large_image" />
+				<meta name="twitter:site" content="@onplug_io" />
+				<meta name="twitter:creator" content="@onplug_io" />
+				<meta name="twitter:title" content="Plug" />
+				<meta
+					name="twitter:description"
+					content="Plug brings trustless automation and scheduling to the entirety of the Ethereum ecosystem with an intent powered framework. The onchain outcomes you want can finally be achieved without being glued to your device."
+				/>
+				<meta
+					name="twitter:image"
+					content="https://onplug.io/opengraph.png"
+				/>
+			</Head>
 
 			<body>
 				<Main />
