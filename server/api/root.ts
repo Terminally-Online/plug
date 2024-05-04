69c288f66330c31ca718d660e837de2e345a754e
nftchance <chance@utc24.io>
2024-05-03 22:13:28 -0500

    fix: deprecate canvas router

--- server/api/root.ts
+++ server/api/root.ts
@@ -1,10 +1,8 @@
 import accountRouter from "@/server/api/routers/account"
-import canvasRouter from "@/server/api/routers/canvas"
 import { createTRPCRouter } from "@/server/api/trpc"
 
 export const appRouter = createTRPCRouter({
-	account: accountRouter,
-	canvas: canvasRouter
+	account: accountRouter
 })
 
 export type AppRouter = typeof appRouter
