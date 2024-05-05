e40a5f47f3cc290840d7a7cf3ed2df5daa141a1c
nftchance <chance@utc24.io>
2024-05-05 01:34:10 -0500

    feat: use static generation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
 import type { AppProps, AppType } from "next/app"
 
 import { Session } from "next-auth"
-import { getSession, SessionProvider } from "next-auth/react"
+// import { getSession, SessionProvider } from "next-auth/react"
 
 import { GoogleTagManager } from "@next/third-parties/google"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
-import { api } from "@/lib/api"
+// import { api } from "@/lib/api"
 import { type NextPageWithLayout } from "@/lib/types"
 
 import "./styles.css"
@@ -23,18 +23,18 @@ const PlugApp: AppType<{
 	return (
 		<>
 			<GoogleTagManager gtmId="GTM-PT3JT2P9" />
-			<SessionProvider session={pageProps.session}>
 				{getLayout(<Component {...pageProps} />)}
-			</SessionProvider>
 			<SpeedInsights />
 		</>
 	)
 }
 
-PlugApp.getInitialProps = async ({ ctx }) => {
-	return {
-		session: await getSession(ctx)
-	}
-}
+export default PlugApp
 
-export default api.withTRPC(PlugApp)
+// PlugApp.getInitialProps = async ({ ctx }) => {
+// 	return {
+// 		session: await getSession(ctx)
+// 	}
+// }
+//
+// export default api.withTRPC(PlugApp)
