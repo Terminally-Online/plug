f3a9141238101c73b64886f097ab28cbed38e3fd
nftchance <chance@utc24.io>
2024-05-06 04:00:57 -0500

    chore: add examples and more feedback cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -83,7 +83,7 @@ const CallToAction: FC<{
 			whileInView={{ opacity: 1, y: 0 }}
 			transition={{ duration: 0.2 }}
 		>
-			<h1 className="text-[36px] font-bold text-white lg:max-w-[85%] lg:text-[72px] lg:text-[96px]">
+			<h1 className="text-[36px] font-bold text-white lg:max-w-[80%] lg:text-[72px] lg:text-[96px]">
 				{text}
 			</h1>
 			<p className="text-[18px] text-white lg:max-w-[60%] lg:text-[24px]">
@@ -148,7 +148,7 @@ const Templates = () => {
 	]
 
 	return (
-		<Container className="my-[90px] flex-col items-center gap-4 lg:my-[180px]">
+		<Container className="my-[90px] flex-col items-center gap-4">
 			<h2 className="text-center text-[28px] font-bold lg:w-[60%] lg:text-[64px] 2xl:w-[50%]">
 				Start today with best-practice templates
 			</h2>
@@ -190,6 +190,530 @@ const Templates = () => {
 		</Container>
 	)
 }
+
+const Examples = () => (
+	<>
+		<Container className="mt-[90px] flex-col items-center gap-4">
+			<h2 className="text-center text-[28px] font-bold lg:w-[60%] lg:text-[64px] 2xl:w-[45%]">
+				A framework that lets you plug-and-play.
+			</h2>
+			<p className="text-center text-[18px] font-light opacity-40 lg:w-[45%] lg:text-[24px]">
+				Declare intents with a state of the art no-code plug builder in
+				seconds. Combine the power of top protocols into a single
+				transaction that are all simulatenously settled.
+			</p>
+		</Container>
+
+		<div
+			className="border-green my-[45px] border-y-[2px] lg:my-[90px]"
+			style={{
+				borderImage: "linear-gradient(45deg, #00EF35, #93DF00) 1"
+			}}
+		>
+			<div className="grid lg:grid-cols-12 lg:grid-rows-2">
+				<div className="lg:col-span-6">
+					<div className="p-4 py-8 lg:p-12 lg:pl-96">
+						<div className="flex flex-col gap-4">
+							<div className="flex flex-row items-center gap-4">
+								<motion.div
+									className="flex h-6 w-6 items-center justify-center rounded-full bg-[#D9D9D9]/40"
+									animate={{
+										background: ["#EAEAEA", "#D9D9D9"]
+									}}
+									transition={{
+										duration: 0.25,
+										repeat: Infinity,
+										repeatType: "reverse",
+										repeatDelay: 2
+									}}
+								>
+									<p className="text-xs">1</p>
+								</motion.div>
+								<Image
+									src="/protocols/plug.png"
+									alt="Plug"
+									width={24}
+									height={24}
+									className="rounded-full"
+								/>
+								<p>
+									Run{" "}
+									<span className="rounded-md bg-[#00EF35]/10 p-2 py-1 text-[#00EF35]">
+										1
+									</span>{" "}
+									time a{" "}
+									<span className="rounded-md bg-[#00EF35]/10 p-2 py-1 text-[#00EF35]">
+										year
+									</span>{" "}
+									.
+								</p>
+							</div>
+							<div className="flex flex-row items-center gap-4">
+								<motion.div
+									className="flex h-6 w-6 items-center justify-center rounded-full bg-[#D9D9D9]/40"
+									animate={{
+										background: ["#EAEAEA", "#D9D9D9"]
+									}}
+									transition={{
+										duration: 0.25,
+										delay: 0.5,
+										repeat: Infinity,
+										repeatType: "reverse",
+										repeatDelay: 2
+									}}
+								>
+									<p className="text-xs">2</p>
+								</motion.div>
+								<Image
+									src="/protocols/uniswap.png"
+									alt="Plug"
+									width={24}
+									height={24}
+									className="rounded-full"
+								/>
+								<p>
+									Can swap{" "}
+									<span className="rounded-md bg-[#00EF35]/10 p-2 py-1 text-[#00EF35]">
+										100
+									</span>{" "}
+									<span className="rounded-md bg-[#00EF35]/10 p-2 py-1 text-[#00EF35]">
+										$USDC
+									</span>{" "}
+									to{" "}
+									<span className="rounded-md bg-[#00EF35]/10 p-2 py-1 text-[#00EF35]">
+										$ETH
+									</span>{" "}
+									.
+								</p>
+							</div>
+							<div className="flex flex-row items-center gap-4">
+								<motion.div
+									className="flex h-6 w-6 items-center justify-center rounded-full bg-[#D9D9D9]/40"
+									animate={{
+										background: ["#EAEAEA", "#D9D9D9"]
+									}}
+									transition={{
+										duration: 0.25,
+										delay: 1,
+										repeat: Infinity,
+										repeatType: "reverse",
+										repeatDelay: 2
+									}}
+								>
+									<p className="text-xs">3</p>
+								</motion.div>
+								<Image
+									src="/protocols/plug.png"
+									alt="Plug"
+									width={24}
+									height={24}
+									className="rounded-full"
+								/>
+								<p>
+									Has{" "}
+									<span className="rounded-md bg-[#00EF35]/10 p-2 py-1 text-[#00EF35]">
+										0.2
+									</span>{" "}
+									<span className="rounded-md bg-[#00EF35]/10 p-2 py-1 text-[#00EF35]">
+										$ETH
+									</span>{" "}
+									or greater.
+								</p>
+							</div>
+							<div className="flex flex-row items-center gap-4">
+								<motion.div
+									className="flex h-6 w-6 items-center justify-center rounded-full bg-[#D9D9D9]/40"
+									animate={{
+										background: ["#EAEAEA", "#D9D9D9"]
+									}}
+									transition={{
+										duration: 0.25,
+										delay: 1.5,
+										repeat: Infinity,
+										repeatType: "reverse",
+										repeatDelay: 2
+									}}
+								>
+									<p className="text-xs">4</p>
+								</motion.div>
+								<Image
+									src="/protocols/ens.png"
+									alt="Plug"
+									width={24}
+									height={24}
+									className="rounded-full"
+								/>
+								<p>
+									Can renew{" "}
+									<span className="rounded-md bg-[#00EF35]/10 p-2 py-1 text-[#00EF35]">
+										nftchance.eth
+									</span>{" "}
+									.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div
+					className="flex border-t-[2px] lg:col-span-5 lg:col-start-7 lg:row-span-2 lg:border-l-[2px] lg:border-t-[0px] lg:pr-24"
+					style={{
+						borderImage:
+							"linear-gradient(45deg, #00EF35, #93DF00) 1"
+					}}
+				>
+					<div className="my-auto p-4 py-8 pr-0 lg:p-16 lg:pl-24">
+						<div className="flex flex-col gap-4">
+							<div className="flex flex-row items-center gap-4">
+								<motion.div
+									className="flex h-6 w-6 items-center justify-center rounded-full bg-[#D9D9D9]/40"
+									animate={{
+										background: ["#EAEAEA", "#D9D9D9"]
+									}}
+									transition={{
+										duration: 0.25,
+										repeat: Infinity,
+										repeatType: "reverse",
+										repeatDelay: 3.5
+									}}
+								>
+									<p className="text-xs">1</p>
+								</motion.div>
+								<Image
+									src="/protocols/plug.png"
+									alt="Plug"
+									width={24}
+									height={24}
+									className="rounded-full"
+								/>
+								<p>
+									Run{" "}
+									<span className="rounded-md bg-[#00EF35]/10 p-2 py-1 text-[#00EF35]">
+										1
+									</span>{" "}
+									time a{" "}
+									<span className="rounded-md bg-[#00EF35]/10 p-2 py-1 text-[#00EF35]">
+										day
+									</span>{" "}
+									.
+								</p>
+							</div>
+							<div className="flex flex-row items-center gap-4">
+								<motion.div
+									className="flex h-6 w-6 items-center justify-center rounded-full bg-[#D9D9D9]/40"
+									animate={{
+										background: ["#EAEAEA", "#D9D9D9"]
+									}}
+									transition={{
+										duration: 0.25,
+										delay: 0.5,
+										repeat: Infinity,
+										repeatType: "reverse",
+										repeatDelay: 3.5
+									}}
+								>
+									<p className="text-xs">2</p>
+								</motion.div>
+								<Image
+									src="/protocols/plug.png"
+									alt="Plug"
+									width={24}
+									height={24}
+									className="rounded-full"
+								/>
+								<p>
+									Run after{" "}
+									<span className="rounded-md bg-[#00EF35]/10 p-2 py-1 text-[#00EF35]">
+										10
+									</span>{" "}
+									<span className="rounded-md bg-[#00EF35]/10 p-2 py-1 text-[#00EF35]">
+										PM
+									</span>{" "}
+									<span className="rounded-md bg-[#00EF35]/10 p-2 py-1 text-[#00EF35]">
+										UTC
+									</span>{" "}
+									.
+								</p>
+							</div>
+							<div className="flex flex-row items-center gap-4">
+								<motion.div
+									className="flex h-6 w-6 items-center justify-center rounded-full bg-[#D9D9D9]/40"
+									animate={{
+										background: ["#EAEAEA", "#D9D9D9"]
+									}}
+									transition={{
+										duration: 0.25,
+										delay: 1,
+										repeat: Infinity,
+										repeatType: "reverse",
+										repeatDelay: 3.5
+									}}
+								>
+									<p className="text-xs">3</p>
+								</motion.div>
+								<Image
+									src="/protocols/plug.png"
+									alt="Plug"
+									width={24}
+									height={24}
+									className="rounded-full"
+								/>
+								<p>
+									Run before{" "}
+									<span className="rounded-md bg-[#00EF35]/10 p-2 py-1 text-[#00EF35]">
+										12/31/2024
+									</span>{" "}
+									.
+								</p>
+							</div>
+							<div className="flex flex-row items-center gap-4">
+								<motion.div
+									className="flex h-6 w-6 items-center justify-center rounded-full bg-[#D9D9D9]/40"
+									animate={{
+										background: ["#EAEAEA", "#D9D9D9"]
+									}}
+									transition={{
+										duration: 0.25,
+										delay: 1.5,
+										repeat: Infinity,
+										repeatType: "reverse",
+										repeatDelay: 3.5
+									}}
+								>
+									<p className="text-xs">4</p>
+								</motion.div>
+								<Image
+									src="/protocols/uniswap.png"
+									alt="Plug"
+									width={24}
+									height={24}
+									className="rounded-full"
+								/>
+								<p>
+									Swap{" "}
+									<span className="rounded-md bg-[#00EF35]/10 p-2 py-1 text-[#00EF35]">
+										36,000
+									</span>{" "}
+									<span className="rounded-md bg-[#00EF35]/10 p-2 py-1 text-[#00EF35]">
+										$USDC
+									</span>{" "}
+									to{" "}
+									<span className="rounded-md bg-[#00EF35]/10 p-2 py-1 text-[#00EF35]">
+										$ETH
+									</span>
+									.
+								</p>
+							</div>
+							<div className="flex flex-row items-center gap-4">
+								<motion.div
+									className="flex h-6 w-6 items-center justify-center rounded-full bg-[#D9D9D9]/40"
+									animate={{
+										background: ["#EAEAEA", "#D9D9D9"]
+									}}
+									transition={{
+										duration: 0.25,
+										delay: 2,
+										repeat: Infinity,
+										repeatType: "reverse",
+										repeatDelay: 3.5
+									}}
+								>
+									<p className="text-xs">5</p>
+								</motion.div>
+								<Image
+									src="/protocols/nouns.png"
+									alt="Plug"
+									width={24}
+									height={24}
+									className="rounded-full"
+								/>
+								<p>
+									Can bid with{" "}
+									<span className="rounded-md bg-[#00EF35]/10 p-2 py-1 text-[#00EF35]">
+										9
+									</span>{" "}
+									<span className="rounded-md bg-[#00EF35]/10 p-2 py-1 text-[#00EF35]">
+										$ETH
+									</span>{" "}
+									.
+								</p>
+							</div>
+							<div className="flex flex-row items-center gap-4">
+								<motion.div
+									className="flex h-6 w-6 items-center justify-center rounded-full bg-[#D9D9D9]/40"
+									animate={{
+										background: ["#EAEAEA", "#D9D9D9"]
+									}}
+									transition={{
+										duration: 0.25,
+										delay: 2.5,
+										repeat: Infinity,
+										repeatType: "reverse",
+										repeatDelay: 3.5
+									}}
+								>
+									<p className="text-xs">6</p>
+								</motion.div>
+								<Image
+									src="/protocols/nouns.png"
+									alt="Plug"
+									width={24}
+									height={24}
+									className="rounded-full"
+								/>
+								<p>Can bid without settling auction.</p>
+							</div>
+							<div className="flex flex-row items-center gap-4">
+								<motion.div
+									className="flex h-6 w-6 items-center justify-center rounded-full bg-[#D9D9D9]/40"
+									animate={{
+										background: ["#EAEAEA", "#D9D9D9"]
+									}}
+									transition={{
+										duration: 0.25,
+										delay: 3,
+										repeat: Infinity,
+										repeatType: "reverse",
+										repeatDelay: 3.5
+									}}
+								>
+									<p className="text-xs">7</p>
+								</motion.div>
+								<Image
+									src="/protocols/nouns.png"
+									alt="Plug"
+									width={24}
+									height={24}
+									className="rounded-full"
+								/>
+								<p>
+									Bid on Noun with{" "}
+									<span className="rounded-md bg-[#00EF35]/10 p-2 py-1 text-[#00EF35]">
+										Pineapple Hat
+									</span>{" "}
+									.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div
+					className="border-t-[2px] lg:col-span-6"
+					style={{
+						borderImage:
+							"linear-gradient(45deg, #00EF35, #93DF00) 1"
+					}}
+				>
+					<div className="p-4 py-8 lg:p-16 lg:pl-96">
+						<div className="flex flex-col gap-4">
+							<div className="flex flex-row items-center gap-4">
+								<motion.div
+									className="flex h-6 w-6 items-center justify-center rounded-full bg-[#D9D9D9]/40"
+									animate={{
+										background: ["#EAEAEA", "#D9D9D9"]
+									}}
+									transition={{
+										duration: 0.25,
+										repeat: Infinity,
+										repeatType: "reverse",
+										repeatDelay: 1.5
+									}}
+								>
+									<p className="text-xs">1</p>
+								</motion.div>
+								<Image
+									src="/protocols/plug.png"
+									alt="Plug"
+									width={24}
+									height={24}
+									className="rounded-full"
+								/>
+								<p>
+									Run{" "}
+									<span className="rounded-md bg-[#00EF35]/10 p-2 py-1 text-[#00EF35]">
+										1
+									</span>{" "}
+									time a{" "}
+									<span className="rounded-md bg-[#00EF35]/10 p-2 py-1 text-[#00EF35]">
+										hour
+									</span>{" "}
+									.
+								</p>
+							</div>
+							<div className="flex flex-row items-center gap-4">
+								<motion.div
+									className="flex h-6 w-6 items-center justify-center rounded-full bg-[#D9D9D9]/40"
+									animate={{
+										background: ["#EAEAEA", "#D9D9D9"]
+									}}
+									transition={{
+										duration: 0.25,
+										delay: 0.5,
+										repeat: Infinity,
+										repeatType: "reverse",
+										repeatDelay: 1.5
+									}}
+								>
+									<p className="text-xs">2</p>
+								</motion.div>
+								<Image
+									src="/protocols/yearn.png"
+									alt="Plug"
+									width={24}
+									height={24}
+									className="rounded-full"
+								/>
+								<p>
+									<span className="rounded-md bg-[#00EF35]/10 p-2 py-1 text-[#00EF35]">
+										$USDC
+									</span>{" "}
+									pool is above{" "}
+									<span className="rounded-md bg-[#00EF35]/10 p-2 py-1 text-[#00EF35]">
+										72%
+									</span>{" "}
+									APY.
+								</p>
+							</div>
+							<div className="flex flex-row items-center gap-4">
+								<motion.div
+									className="flex h-6 w-6 items-center justify-center rounded-full bg-[#D9D9D9]/40"
+									animate={{
+										background: ["#EAEAEA", "#D9D9D9"]
+									}}
+									transition={{
+										duration: 0.25,
+										delay: 1,
+										repeat: Infinity,
+										repeatType: "reverse",
+										repeatDelay: 1.5
+									}}
+								>
+									<p className="text-xs">3</p>
+								</motion.div>
+								<Image
+									src="/protocols/yearn.png"
+									alt="Plug"
+									width={24}
+									height={24}
+									className="rounded-full"
+								/>
+								<p>
+									Can deposit{" "}
+									<span className="rounded-md bg-[#00EF35]/10 p-2 py-1 text-[#00EF35]">
+										10,000
+									</span>{" "}
+									<span className="rounded-md bg-[#00EF35]/10 p-2 py-1 text-[#00EF35]">
+										$USDC
+									</span>{" "}
+									.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</>
+)
 
 const Value = () => {
 	const animation = {
@@ -1467,7 +1991,7 @@ const Value = () => {
 
 const Letter = () => (
 	<>
-		<Container className="my-[90px] flex-col items-center gap-4 lg:my-[180px]">
+		<Container className="my-[90px] flex-col items-center gap-4">
 			<motion.h2
 				className="text-center text-[28px] font-bold lg:w-[65%] lg:text-[64px]"
 				initial={{ opacity: 0, y: 20 }}
@@ -1525,16 +2049,11 @@ const Letter = () => (
 							composability between the many options.
 						</p>
 						<p>
-							You can choose to stay inside and get worse
+							You can choose to stay in the past and get worse
 							execution with unexpected outcomes or you can use
 							Plug and always be certain the outcomes will be
 							generated when your conditions have been met.
 						</p>
-						<p>
-							Remember how your mom told you to go outside and
-							play with your friends?
-						</p>
-						<p>We’re here to do the same. It’s nice out there.</p>
 						<p>Love,</p>
 						<Image
 							src="/landing/signature.svg"
@@ -1597,7 +2116,7 @@ const FrequentlyAskedQuestion: FC<{ text: string; description: string }> = ({
 
 const FrequentlyAskedQuestions = () => (
 	<Container>
-		<div className="mb-[90px] grid lg:mb-[180px] lg:grid-cols-12">
+		<div className="my-[90px] grid lg:grid-cols-12">
 			<h3 className="mb-8 text-[28px] font-bold lg:col-span-4 lg:col-start-2 lg:mb-0 lg:w-[60%] lg:text-[64px] 2xl:w-[50%]">
 				Frequently Asked Questions
 			</h3>
@@ -1728,7 +2247,7 @@ const Vision = () => {
 				>
 					<div className="flex flex-wrap items-end justify-end">
 						{immutableFactoryBytecode
-							.slice(0, 180)
+							.slice(0, 240)
 							.replaceAll(" ", "")
 							.split("")
 							.map((char, index) => {
@@ -1760,6 +2279,8 @@ const Vision = () => {
 								)
 							})}
 					</div>
+					<div className="absolute bottom-1/2 left-0 right-0 top-0 bg-gradient-to-b from-[#FBFBFB]/0 to-[#FBFBFB] lg:bottom-1/4" />
+					<div className="absolute bottom-0 left-0 right-0 top-1/2 bg-[#FBFBFB] lg:top-3/4" />
 				</InfoCard>
 				<InfoCard
 					text={
@@ -1788,21 +2309,22 @@ const Page = () => {
 			<Navbar />
 			<Hero />
 			<Steps />
-			<Templates />
+			<Examples />
 			<Value />
 			<CallToAction
 				text="Get what you want from every transaction."
 				description="Simultaneous settlement ensures transactions only execute when the conditions and expected outcomes you set in your intent can be met. No fees are paid and tokens move unless everything happens as expected."
 				button="Get Started"
 			/>
-			<Letter />
-			<FrequentlyAskedQuestions />
+			<Templates />
 			<Vision />
 			<CallToAction
 				text="Blockchains weren’t built for humans."
 				description="Instead of being trapped inside waiting to click the buttons you can go outside and live the life you want without worrying about missing every opportunity. Your capital can finally manage itself."
 				button="Get Early Access"
 			/>
+			<Letter />
+			<FrequentlyAskedQuestions />
 			<Footer />
 		</>
 	)
