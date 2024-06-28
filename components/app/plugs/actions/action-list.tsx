e54f621c6cc0f01defd2f3bd8c2cef4403a65467
nftchance <chance@utc24.io>
2024-06-27 20:25:39 -0500

    fix: type build error

--- components/app/plugs/actions/action-list.tsx
+++ components/app/plugs/actions/action-list.tsx
@@ -6,11 +6,7 @@ import { categories } from "@/lib/constants"
 export const ActionList = () => (
 	<>
 		{Object.keys(categories).map(categoryName => (
-			<ActionListItem
-				key={categoryName}
-				categoryName={categoryName}
-				category={categories[categoryName]}
-			/>
+			<ActionListItem key={categoryName} categoryName={categoryName} />
 		))}
 	</>
 )
