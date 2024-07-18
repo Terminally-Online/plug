4a4daba072d70de17cd28a82959a19bc5df1bde7
nftchance <chance@utc24.io>
2024-07-18 15:54:41 -0500

    chore: add security headers

--- next.config.js
+++ next.config.js
@@ -2,39 +2,74 @@
 
 const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin")
 
+const remotePatterns = [
+	{
+		protocol: "https",
+		hostname: "assets.smold.app"
+	},
+	{
+		protocol: "https",
+		hostname: "ipfs.io"
+	},
+	{
+		protocol: "https",
+		hostname: "**",
+	},
+]
+
+const headers = [
+	{
+		key: "X-Frame-Options",
+		value: "SAMEORIGIN"
+	},
+	{
+		key: "X-XSS-Protection",
+		value: "1; mode=block"
+	},
+	{
+		key: "X-Content-Type-Options",
+		value: "nosniff"
+	},
+	{
+		key: "Referrer-Policy",
+		value: "strict-origin-when-cross-origin"
+	},
+	{
+		key: "Content-Security-Policy",
+		value: "frame-ancestors 'none'"
+	},
+]
+
 const nextConfig = {
 	poweredByHeader: false,
 	trailingSlash: true,
 	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "assets.smold.app"
-			},
-			{
-				protocol: "https",
-				hostname: "ipfs.io"
-			},
-			{
-				protocol: "https",
-				hostname: "**",
-			},
-		]
+		remotePatterns
 	},
 	/** We run eslint as a separate task in CI */
 	eslint: { ignoreDuringBuilds: !!process.env.CI },
-	webpack: config => {
-		config.externals.push("pino-pretty", "lokijs", "encoding")
-		config.externals.push({
-			"utf-8-validate": "commonjs utf-8-validate",
-			bufferutil: "commonjs bufferutil"
-		})
+	headers: () => {
+		return [{
+			source: "/(.*)",
+			headers
+		}]
+	},
+	webpack: (config) => {
+		config.externals.push(
+			"pino-pretty",
+			"lokijs",
+			"encoding",
+			{
+				"utf-8-validate": "commonjs utf-8-validate",
+				"bufferutil": "commonjs bufferutil"
+			}
+		)
 
 		config.resolve.plugins.push(new TsconfigPathsPlugin({}))
 
 		return config
 	},
-	async redirects() {
+	redirects: async () => {
 		return [
 			{
 				source: "/waitlist",
