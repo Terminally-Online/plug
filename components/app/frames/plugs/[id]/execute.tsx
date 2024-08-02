39963b60be5daa05c6d3afb39eab7f55a177dbb4
nftchance <chance@utc24.io>
2024-08-01 21:27:07 -0500

    fix: remove socket imports that no longer exist

--- components/app/frames/plugs/[id]/execute.tsx
+++ components/app/frames/plugs/[id]/execute.tsx
@@ -3,11 +3,9 @@ import { RanFrame } from "./execute/ran"
 import { RunFrame } from "./execute/run"
 import { RunningFrame } from "./execute/running"
 import { ScheduleFrame } from "./execute/schedule"
-import { SocketFrame } from "./execute/socket"
 
 export const ExecuteFrame = () => (
 	<>
-		<SocketFrame />
 		<ChainFrame />
 		<ScheduleFrame />
 		<RunFrame />
