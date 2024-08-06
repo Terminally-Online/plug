7bab25e763aaf0bd40c3793c80c1ea8b8f2c81a4
nftchance <chance@utc24.io>
2024-08-06 17:37:29 -0500

    chore: stat card style guiding

--- components/app/cards/stat.tsx
+++ components/app/cards/stat.tsx
@@ -2,7 +2,10 @@ import { FC, PropsWithChildren } from "react"
 
 export const StatCard: FC<PropsWithChildren> = ({ children, ...props }) => {
 	return (
-		<div className="w-full rounded-[16px] bg-grayscale-0 p-4" {...props}>
+		<div
+			className="w-full rounded-[16px] bg-gradient-to-tr from-grayscale-100 to-grayscale-0 p-4"
+			{...props}
+		>
 			{children}
 		</div>
 	)
