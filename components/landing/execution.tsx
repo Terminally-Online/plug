c420ef312be9e3fa50c50ff3615e0da8b91a4f5a
nftchance <chance@utc24.io>
2024-09-02 20:03:15 -0500

    feat: final execution

--- components/landing/execution.tsx
+++ components/landing/execution.tsx
@@ -14,8 +14,8 @@ const actions = [
 			protocol: "plug",
 			elements: (
 				<>
-					Run <span className="rounded-md bg-[#00E100]/10 p-2 py-1 text-[#00E100]">1</span> time a{" "}
-					<span className="rounded-md bg-[#00E100]/10 p-2 py-1 text-[#00E100]">year</span> .
+					Run <span className="rounded-sm bg-[#00E100]/10 p-2 py-1 text-[#00E100]">1</span> time a{" "}
+					<span className="rounded-sm bg-[#00E100]/10 p-2 py-1 text-[#00E100]">year</span> .
 				</>
 			)
 		},
@@ -23,9 +23,9 @@ const actions = [
 			protocol: "uniswap",
 			elements: (
 				<>
-					Can swap <span className="rounded-md bg-[#00E100]/10 p-2 py-1 text-[#00E100]">100</span>{" "}
-					<span className="rounded-md bg-[#00E100]/10 p-2 py-1 text-[#00E100]">$USDC</span> to{" "}
-					<span className="rounded-md bg-[#00E100]/10 p-2 py-1 text-[#00E100]">$ETH</span> .
+					Can swap <span className="rounded-sm bg-[#00E100]/10 p-2 py-1 text-[#00E100]">100</span>{" "}
+					<span className="rounded-sm bg-[#00E100]/10 p-2 py-1 text-[#00E100]">$USDC</span> to{" "}
+					<span className="rounded-sm bg-[#00E100]/10 p-2 py-1 text-[#00E100]">$ETH</span> .
 				</>
 			)
 		},
@@ -33,8 +33,8 @@ const actions = [
 			protocol: "plug",
 			elements: (
 				<>
-					Has <span className="rounded-md bg-[#00E100]/10 p-2 py-1 text-[#00E100]">0.2</span>{" "}
-					<span className="rounded-md bg-[#00E100]/10 p-2 py-1 text-[#00E100]">$ETH</span> or greater.
+					Has <span className="rounded-sm bg-[#00E100]/10 p-2 py-1 text-[#00E100]">0.2</span>{" "}
+					<span className="rounded-sm bg-[#00E100]/10 p-2 py-1 text-[#00E100]">$ETH</span> or greater.
 				</>
 			)
 		},
@@ -42,7 +42,7 @@ const actions = [
 			protocol: "ens",
 			elements: (
 				<>
-					Can renew <span className="rounded-md bg-[#00E100]/10 p-2 py-1 text-[#00E100]">nftchance.eth</span>{" "}
+					Can renew <span className="rounded-sm bg-[#00E100]/10 p-2 py-1 text-[#00E100]">nftchance.eth</span>{" "}
 					.
 				</>
 			)
@@ -53,8 +53,8 @@ const actions = [
 			protocol: "plug",
 			elements: (
 				<>
-					Run <span className="rounded-md bg-[#00E100]/10 p-2 py-1 text-[#00E100]">1</span> time a{" "}
-					<span className="rounded-md bg-[#00E100]/10 p-2 py-1 text-[#00E100]">day</span> .
+					Run <span className="rounded-sm bg-[#00E100]/10 p-2 py-1 text-[#00E100]">1</span> time a{" "}
+					<span className="rounded-sm bg-[#00E100]/10 p-2 py-1 text-[#00E100]">day</span> .
 				</>
 			)
 		},
@@ -62,9 +62,9 @@ const actions = [
 			protocol: "plug",
 			elements: (
 				<>
-					Run after <span className="rounded-md bg-[#00E100]/10 p-2 py-1 text-[#00E100]">10</span>{" "}
-					<span className="rounded-md bg-[#00E100]/10 p-2 py-1 text-[#00E100]">PM</span>{" "}
-					<span className="rounded-md bg-[#00E100]/10 p-2 py-1 text-[#00E100]">UTC</span> .
+					Run after <span className="rounded-sm bg-[#00E100]/10 p-2 py-1 text-[#00E100]">10</span>{" "}
+					<span className="rounded-sm bg-[#00E100]/10 p-2 py-1 text-[#00E100]">PM</span>{" "}
+					<span className="rounded-sm bg-[#00E100]/10 p-2 py-1 text-[#00E100]">UTC</span> .
 				</>
 			)
 		},
@@ -72,7 +72,7 @@ const actions = [
 			protocol: "plug",
 			elements: (
 				<>
-					Run before <span className="rounded-md bg-[#00E100]/10 p-2 py-1 text-[#00E100]">12/31/2024</span> .
+					Run before <span className="rounded-sm bg-[#00E100]/10 p-2 py-1 text-[#00E100]">12/31/2024</span> .
 				</>
 			)
 		},
@@ -80,9 +80,9 @@ const actions = [
 			protocol: "plug",
 			elements: (
 				<>
-					Swap <span className="rounded-md bg-[#00E100]/10 p-2 py-1 text-[#00E100]">36,000</span>{" "}
-					<span className="rounded-md bg-[#00E100]/10 p-2 py-1 text-[#00E100]">$USDC</span> to{" "}
-					<span className="rounded-md bg-[#00E100]/10 p-2 py-1 text-[#00E100]">$ETH</span>.
+					Swap <span className="rounded-sm bg-[#00E100]/10 p-2 py-1 text-[#00E100]">36,000</span>{" "}
+					<span className="rounded-sm bg-[#00E100]/10 p-2 py-1 text-[#00E100]">$USDC</span> to{" "}
+					<span className="rounded-sm bg-[#00E100]/10 p-2 py-1 text-[#00E100]">$ETH</span> .
 				</>
 			)
 		},
@@ -90,8 +90,8 @@ const actions = [
 			protocol: "nouns",
 			elements: (
 				<>
-					Can bid with <span className="rounded-md bg-[#00E100]/10 p-2 py-1 text-[#00E100]">9</span>{" "}
-					<span className="rounded-md bg-[#00E100]/10 p-2 py-1 text-[#00E100]">$ETH</span> .
+					Can bid with <span className="rounded-sm bg-[#00E100]/10 p-2 py-1 text-[#00E100]">9</span>{" "}
+					<span className="rounded-sm bg-[#00E100]/10 p-2 py-1 text-[#00E100]">$ETH</span> .
 				</>
 			)
 		},
@@ -100,7 +100,7 @@ const actions = [
 			elements: (
 				<>
 					Bid on Noun with{" "}
-					<span className="rounded-md bg-[#00E100]/10 p-2 py-1 text-[#00E100]">Pineapple Hat</span> .
+					<span className="rounded-sm bg-[#00E100]/10 p-2 py-1 text-[#00E100]">Pineapple Hat</span> .
 				</>
 			)
 		}
@@ -110,8 +110,8 @@ const actions = [
 			protocol: "plug",
 			elements: (
 				<>
-					Run <span className="rounded-md bg-[#00E100]/10 p-2 py-1 text-[#00E100]">1</span> time a{" "}
-					<span className="rounded-md bg-[#00E100]/10 p-2 py-1 text-[#00E100]">hour</span> .
+					Run <span className="rounded-sm bg-[#00E100]/10 p-2 py-1 text-[#00E100]">1</span> time a{" "}
+					<span className="rounded-sm bg-[#00E100]/10 p-2 py-1 text-[#00E100]">hour</span> .
 				</>
 			)
 		},
@@ -119,8 +119,8 @@ const actions = [
 			protocol: "yearn",
 			elements: (
 				<>
-					<span className="rounded-md bg-[#00E100]/10 p-2 py-1 text-[#00E100]">$USDC</span> pool is above{" "}
-					<span className="rounded-md bg-[#00E100]/10 p-2 py-1 text-[#00E100]">72%</span> APY.
+					<span className="rounded-sm bg-[#00E100]/10 p-2 py-1 text-[#00E100]">$USDC</span> pool is above{" "}
+					<span className="rounded-sm bg-[#00E100]/10 p-2 py-1 text-[#00E100]">72%</span> APY.
 				</>
 			)
 		},
@@ -128,8 +128,8 @@ const actions = [
 			protocol: "yearn",
 			elements: (
 				<>
-					Can deposit <span className="rounded-md bg-[#00E100]/10 p-2 py-1 text-[#00E100]">10,000</span>{" "}
-					<span className="rounded-md bg-[#00E100]/10 p-2 py-1 text-[#00E100]">$USDC</span> .
+					Can deposit <span className="rounded-sm bg-[#00E100]/10 p-2 py-1 text-[#00E100]">10,000</span>{" "}
+					<span className="rounded-sm bg-[#00E100]/10 p-2 py-1 text-[#00E100]">$USDC</span> .
 				</>
 			)
 		}
@@ -143,71 +143,102 @@ const ExecutionAction: FC<PropsWithChildren<{ index: number; indexes: number; pr
 	children
 }) => {
 	return (
-		<motion.div
-			className="relative flex w-full flex-row items-center gap-4 rounded-lg"
-			initial={{ opacity: 0, transform: "translateY(20px)" }}
-			animate={{ opacity: 1, transform: "translateY(0px)" }}
-			transition={{
-				duration: 0.2,
-				delay: index * 0.05,
-				repeat: Infinity,
-				repeatType: "reverse",
-				repeatDelay: 3 * indexes
-			}}
-		>
+		<>
 			<motion.div
-				className="bg-gradient-radial absolute inset-0 rounded-lg from-plug-green via-plug-yellow to-transparent opacity-0 blur-lg filter"
-				initial={{ opacity: 0, rotate: 0 }}
-				animate={{
-					opacity: [0, 0.1, 0.2, 0],
-					background: [
-						"radial-gradient(circle at 0% 0%, #00E100 0%, #A3F700 50%, transparent 100%)",
-						"radial-gradient(circle at 100% 0%, #00E100 0%, #A3F700 50%, transparent 100%)",
-						"radial-gradient(circle at 100% 100%, #00E100 0%, #A3F700 50%, transparent 100%)",
-						"radial-gradient(circle at 0% 100%, #00E100 0%, #A3F700 50%, transparent 100%)",
-						"radial-gradient(circle at 0% 0%, #00E100 0%, #A3F700 50%, transparent 100%)"
-					]
-				}}
+				className="relative flex w-full flex-row items-center gap-4 rounded-lg"
+				initial={{ opacity: 0, transform: "translateY(20px)" }}
+				animate={{ opacity: 1, transform: "translateY(0px)" }}
 				transition={{
-					duration: 2,
-					ease: "linear",
-					delay: 0.1 + index * 2,
-					repeat: Infinity
+					duration: 0.2,
+					delay: index * 0.05,
+					repeat: Infinity,
+					repeatType: "reverse",
+					repeatDelay: 3 * indexes
 				}}
-			/>
-
-			<motion.div
-				className="bg-gradient-radial absolute inset-0 rounded-lg from-plug-green via-plug-yellow to-transparent opacity-0"
-				initial={{ opacity: 0, rotate: 0 }}
-				animate={{
-					opacity: [0, 1, 1, 0],
-					background: [
-						"radial-gradient(circle at 0% 0%, #00E100 0%, #A3F700 50%, transparent 100%)",
-						"radial-gradient(circle at 100% 0%, #00E100 0%, #A3F700 50%, transparent 100%)",
-						"radial-gradient(circle at 100% 100%, #00E100 0%, #A3F700 50%, transparent 100%)",
-						"radial-gradient(circle at 0% 100%, #00E100 0%, #A3F700 50%, transparent 100%)",
-						"radial-gradient(circle at 0% 0%, #00E100 0%, #A3F700 50%, transparent 100%)"
-					]
-				}}
-				transition={{
-					duration: 2,
-					ease: "linear",
-					delay: 0.1 + index * 2,
-					repeat: Infinity
-				}}
-			/>
-
-			<div className="relative z-10 m-[2px] flex w-full flex-row items-center gap-4 rounded-lg bg-white p-3">
-				<Image
-					src={`/protocols/${protocol}.png`}
-					alt={formatTitle(protocol)}
-					width={48}
-					height={48}
-					className="h-6 w-6 rounded-sm"
+			>
+				<motion.div
+					className="bg-gradient-radial absolute inset-0 rounded-lg from-plug-green via-plug-yellow to-white opacity-0 blur-lg filter"
+					initial={{ opacity: 0 }}
+					animate={{
+						opacity: [0, 0.1, 0.2, 0],
+						background: [
+							"radial-gradient(circle at 0% 0%, #00E100 0%, #A3F700 50%, #FFFFFF 100%)",
+							"radial-gradient(circle at 100% 0%, #00E100 0%, #A3F700 50%, #FFFFFF 100%)",
+							"radial-gradient(circle at 100% 100%, #00E100 0%, #A3F700 50%, #FFFFFF 100%)",
+							"radial-gradient(circle at 0% 100%, #00E100 0%, #A3F700 50%, #FFFFFF 100%)",
+							"radial-gradient(circle at 0% 0%, #00E100 0%, #A3F700 50%, #FFFFFF 100%)"
+						]
+					}}
+					transition={{
+						duration: 2,
+						ease: "linear",
+						delay: 0.1 + index * 2,
+						repeat: Infinity
+					}}
 				/>
-				<p className="font-bold text-black/40">{children}</p>
-			</div>
-		</motion.div>
+
+				<motion.div
+					className="bg-gradient-radial absolute inset-0 rounded-lg from-plug-green via-plug-yellow to-white opacity-0"
+					initial={{ opacity: 0 }}
+					animate={{
+						opacity: [0, 1, 1, 0],
+						background: [
+							"radial-gradient(circle at 0% 0%, #00E100 0%, #A3F700 50%, #FFFFFF 100%)",
+							"radial-gradient(circle at 100% 0%, #00E100 0%, #A3F700 50%, #FFFFFF 100%)",
+							"radial-gradient(circle at 100% 100%, #00E100 0%, #A3F700 50%, #FFFFFF 100%)",
+							"radial-gradient(circle at 0% 100%, #00E100 0%, #A3F700 50%, #FFFFFF 100%)",
+							"radial-gradient(circle at 0% 0%, #00E100 0%, #A3F700 50%, #FFFFFF 100%)"
+						]
+					}}
+					transition={{
+						duration: 2,
+						ease: "linear",
+						delay: 0.1 + index * 2,
+						repeat: Infinity
+					}}
+				/>
+
+				<div className="relative z-10 m-[2px] flex w-full flex-row items-center gap-4 rounded-lg bg-white p-3">
+					<Image
+						src={`/protocols/${protocol}.png`}
+						alt={formatTitle(protocol)}
+						width={48}
+						height={48}
+						className="h-6 w-6 rounded-sm"
+					/>
+					<p className="font-bold text-black/40">{children}</p>
+				</div>
+			</motion.div>
+
+			{index !== indexes && (
+				<motion.div
+					className="h-4 w-[2px]"
+					initial={{ opacity: 0, transform: "translateY(20px)" }}
+					animate={{ opacity: 1, transform: "translateY(0px)" }}
+					transition={{
+						duration: 0.2,
+						delay: index * 0.05,
+						repeat: Infinity,
+						repeatType: "reverse",
+						repeatDelay: 3 * indexes
+					}}
+				>
+					<motion.div
+						className="h-full w-full bg-grayscale-100"
+						initial={{ background: "linear-gradient(30deg, #EBECEC, #EBECEC)" }}
+						animate={{ background: "linear-gradient(30deg, #00E100, #A3F700)" }}
+						transition={{
+							duration: 0.2,
+							ease: "linear",
+							delay: 2 + index * 2,
+							repeat: Infinity,
+							repeatType: "reverse",
+							repeatDelay: 3 * indexes
+						}}
+					/>
+				</motion.div>
+			)}
+		</>
 	)
 }
 
@@ -226,7 +257,7 @@ const ExecutionActions = () => {
 	}, [currentActionSet])
 
 	return (
-		<div className="flex flex-col items-center gap-2">
+		<div className="flex flex-col items-center">
 			{actions[currentActionSet].map((action, index) => (
 				<ExecutionAction
 					key={`${currentActionSet}-${index}`}
