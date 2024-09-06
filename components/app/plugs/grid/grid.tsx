d97678fcbe17e1b7991232061b8a5c701b6ec442
nftchance <chance@utc24.io>
2024-09-05 23:08:12 -0500

    fix: plug grid plugs error

--- components/app/plugs/grid/grid.tsx
+++ components/app/plugs/grid/grid.tsx
@@ -7,19 +7,23 @@ import { Animate, PlugGridItem } from "@/components"
 type Props = React.HTMLAttributes<HTMLDivElement> & {
 	id: string
 	from: string
-	plugs: Array<Workflow | undefined>
+	plugs: Array<Workflow | undefined> | undefined
 	count?: number
 	handleReset?: () => void
 }
 
-export const PlugGrid: FC<Props> = ({ id, from, plugs, count, handleReset, ...props }) => (
-	<div {...props}>
-		<Animate.Grid>
-			{plugs.slice(0, count || plugs.length).map((plug, index) => (
-				<Animate.ListItem key={index}>
-					<PlugGridItem id={id} from={from} plug={plug} />
-				</Animate.ListItem>
-			))}
-		</Animate.Grid>
-	</div>
-)
+export const PlugGrid: FC<Props> = ({ id, from, plugs, count, handleReset, ...props }) => {
+	if (plugs === undefined) return null
+
+	return (
+		<div {...props}>
+			<Animate.Grid>
+				{plugs.slice(0, count || plugs.length).map((plug, index) => (
+					<Animate.ListItem key={index}>
+						<PlugGridItem id={id} from={from} plug={plug} />
+					</Animate.ListItem>
+				))}
+			</Animate.Grid>
+		</div>
+	)
+}
