e8085c518a6618375517c1b82493ea1f996819e3
nftchance <chance@utc24.io>
2024-09-09 21:08:12 -0500

    fix: bad copy

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,27 @@
+import Head from "next/head"
+
 import { Blocks, CallToAction, Hero, LandingFooter, Light, Transactions, Vision } from "@/components"
 
 const Page = () => (
-	<div className="overflow-x-hidden">
-		<Hero />
-		{/* <Demo /> */}
-		<Light />
-		<Transactions />
-		<CallToAction
-			text="Operate at a level your friends only dream of."
-			description="Experience extraordinary results, elevate your onchain performance, leave the rest of the market behind, and change the scale of your goals."
-			button="Get Started"
-		/>
-		<Vision />
-		<Blocks />
-		<LandingFooter />
-	</div>
+	<>
+		<Head>
+			<title>Plug</title>
+		</Head>
+
+		<div className="overflow-x-hidden">
+			<Hero />
+			<Light />
+			<Transactions />
+			<CallToAction
+				text="Operate at a level your friends only dream of."
+				description="If you can't beat them, join them. Embrace the future of onchain activity and supercharge your potential with only a couple of clicks and a few minutes."
+				button="Get Started"
+			/>
+			<Vision />
+			<Blocks />
+			<LandingFooter />
+		</div>
+	</>
 )
 
 export default Page
