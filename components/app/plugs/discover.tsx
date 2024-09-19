cf0526ad1d556bbff88daed178c212c2bf9969e3
nftchance <chance@utc24.io>
2024-09-19 11:46:53 -0500

    chore: temporary fix for discovery

--- components/app/plugs/discover.tsx
+++ components/app/plugs/discover.tsx
@@ -1,4 +1,4 @@
-import { FC, HTMLAttributes, useMemo, useState } from "react"
+import { FC, HTMLAttributes, useEffect, useMemo, useState } from "react"
 
 import { useMotionValueEvent, useScroll } from "framer-motion"
 import { SearchIcon } from "lucide-react"
@@ -15,46 +15,31 @@ export const PlugsDiscover: FC<HTMLAttributes<HTMLDivElement> & { index?: number
 	className,
 	...props
 }) => {
-	const { scrollYProgress } = useScroll()
+	// const { scrollYProgress } = useScroll()
 	const { search, tag, handleSearch, handleTag } = useSearch()
 
-	const [plugs, setPlugs] = useState<{
-		count?: number
-		plugs: Array<Workflow>
-	}>({ plugs: [] })
+	// const [plugs, setPlugs] = useState<{
+	// 	count?: number
+	// 	plugs: Array<Workflow>
+	// }>({ plugs: [] })
 
-	const { fetchNextPage, isLoading } = api.plug.infinite.useInfiniteQuery(
-		{
-			search,
-			tag,
-			limit: 40
-		},
-		{
-			getNextPageParam(lastPage) {
-				return lastPage.nextCursor
-			},
-			onSuccess(data) {
-				setPlugs(() => ({
-					count: data.pages[data.pages.length - 1].count,
-					plugs: data.pages.flatMap(page => page.plugs)
-				}))
-			}
-		}
-	)
+	const { data: plugs } = api.plug.all.useQuery({
+		target: "others"
+	})
 
 	const visiblePlugs = useMemo(() => {
-		if (plugs === undefined || plugs.count === 0) return Array(12).fill(undefined)
-		return plugs.plugs
+		if (plugs === undefined || plugs.length === 0) return Array(12).fill(undefined)
+		return plugs
 	}, [plugs])
 
-	useMotionValueEvent(scrollYProgress, "change", latest => {
-		if (!plugs || isLoading || latest < 0.8) return
-		if ((plugs.count ?? 0) > plugs.plugs.length) fetchNextPage()
-	})
+	// useMotionValueEvent(scrollYProgress, "change", latest => {
+	// 	if (!plugs || isLoading || latest < 0.8) return
+	// 	if ((plugs.count ?? 0) > plugs.plugs.length) fetchNextPage()
+	// })
 
 	return (
 		<div className={cn("relative flex h-full flex-col gap-2", className)} {...props}>
-			{plugs && plugs.plugs.length > 0 && (
+			{plugs && plugs.length > 0 && (
 				<Container>
 					<Search
 						icon={<SearchIcon size={14} className="opacity-60" />}
@@ -69,7 +54,7 @@ export const PlugsDiscover: FC<HTMLAttributes<HTMLDivElement> & { index?: number
 			{visiblePlugs.some(plug => Boolean(plug)) && <Tags tag={tag} handleTag={handleTag} />}
 
 			<Callout.EmptySearch
-				isEmpty={search !== "" && plugs && plugs.count === 0}
+				isEmpty={(search !== "" && plugs && plugs.length === 0) || false}
 				search={search}
 				handleSearch={handleSearch}
 			/>
@@ -78,7 +63,7 @@ export const PlugsDiscover: FC<HTMLAttributes<HTMLDivElement> & { index?: number
 				<PlugGrid index={index} className="mb-4" from={VIEW_KEYS.DISCOVER} plugs={visiblePlugs} />
 			</Container>
 
-			<Callout.EmptyPlugs index={index} isEmpty={search === "" && plugs && plugs.count === 0} />
+			<Callout.EmptyPlugs index={index} isEmpty={(search === "" && plugs && plugs.length === 0) || false} />
 		</div>
 	)
 }
