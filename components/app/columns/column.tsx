e8318aa27abb08c5200f0d1fd3cf8976f5dff64b
nftchance <chance@utc24.io>
2024-10-01 18:45:52 -0500

    fix: relative position of column removed

--- components/app/columns/column.tsx
+++ components/app/columns/column.tsx
@@ -210,7 +210,7 @@ export const ConsoleColumn: FC<{
 							</div>
 
 							{/* TODO(#416): Have the column.frame check to disable the scroll when a frame is open. Need a better solution. */}
-							<div className={cn("relative h-full", !column.frame && "overflow-y-scroll")}>
+							<div className={cn("h-full", !column.frame && "overflow-y-scroll")}>
 								{key === VIEW_KEYS.AUTHENTICATE ? (
 									<ColumnAuthenticate index={index} />
 								) : key === VIEW_KEYS.ADD ? (
