5e2a8b28d880ffba1b8c57c4be9b83229ee189df
nftchance <chance@utc24.io>
2024-10-04 11:02:31 -0500

    chore: proper column row left margin

--- components/app/columns/column-row.tsx
+++ components/app/columns/column-row.tsx
@@ -3,10 +3,11 @@ import { AnimatePresence } from "framer-motion"
 import { DragDropContext, Droppable, DropResult } from "@hello-pangea/dnd"
 
 import { ColumnAdd, ConsoleColumn } from "@/components"
-import { MOBILE_INDEX } from "@/lib"
-import { useColumns } from "@/state"
+import { cn, MOBILE_INDEX } from "@/lib"
+import { useColumns, useSidebar } from "@/state"
 
 export const ConsoleColumnRow = () => {
+	const { is } = useSidebar()
 	const { columns, move } = useColumns()
 
 	const onDragEnd = (result: DropResult) => {
@@ -19,7 +20,12 @@ export const ConsoleColumnRow = () => {
 	}
 
 	return (
-		<div className="flex h-full flex-row overflow-x-auto overflow-y-hidden">
+		<div
+			className={cn(
+				"flex h-full flex-row overflow-x-auto overflow-y-hidden",
+				(is.searching || is.viewingAs) === false && "ml-2"
+			)}
+		>
 			<DragDropContext onDragEnd={onDragEnd}>
 				<Droppable droppableId="droppable" direction="horizontal">
 					{provided => (
