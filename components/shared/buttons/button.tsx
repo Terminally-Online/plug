2ec056525643767ee8bc784b472187348542d4ee
nftchance <chance@utc24.io>
2024-10-04 21:10:18 -0500

    feat: jelly buttons

--- components/shared/buttons/button.tsx
+++ components/shared/buttons/button.tsx
@@ -13,7 +13,7 @@ type Props = {
 
 const variants: Record<NonNullable<Props["variant"]>, string> = {
 	primary:
-		"relative bg-gradient-to-tr from-plug-green to-plug-yellow text-white before:absolute before:inset-0 before:bg-gradient-to-tr before:from-plug-green before:to-plug-yellow before:w-full before:h-full before:blur-sm before:z-[-1] hover:before:blur-md transition-all duration-200 ease-in-out before:transition-all before:duration-200 before:ease-in-out whitespace-nowrap shadow-[0_0_16px_rgba(0,255,0,1)] hover:shadow-[0_0_8px_rgba(0,255,0,1)]",
+		"relative bg-gradient-to-tr from-plug-green to-plug-yellow text-white before:absolute before:inset-0 before:bg-gradient-to-tr before:from-plug-green before:to-plug-yellow before:w-full before:h-full before:blur-sm before:z-[-1] hover:before:blur-md transition-all duration-200 ease-in-out before:transition-all before:duration-200 before:ease-in-out whitespace-nowrap",
 	secondary:
 		"border-[1px] border-grayscale-0 text-black hover:border-white hover:bg-grayscale-100 items-center flex justify-center text-opacity-60 whitespace-nowrap [&.active]:bg-grayscale-0 [&.active]:text-opacity-100 [&.active]:hover:bg-grayscale-100 [&.active]:hover:border-grayscale-0",
 	white: "bg-white text-black hover:bg-opacity-80",
@@ -44,6 +44,7 @@ export const Button: FC<Props> = ({
 
 	if (onClick || disabled)
 		return (
+
 			<button
 				{...props}
 				type="button"
@@ -51,6 +52,33 @@ export const Button: FC<Props> = ({
 				className={cn(variants[variant], sizings[sizing], base, className)}
 				disabled={disabled}
 			>
+				{variant === "primary" && <>
+					<div className={cn(
+						"absolute inset-0 shadow-[inset_-4px_0_4px_0_rgba(255,255,255,.5)]", 
+						sizing === "sm" && "rounded-sm", 
+						sizing === "md" && "rounded-md", 
+						sizing === "lg" && "rounded-lg"
+					)} />
+					<div className={cn(
+						"absolute inset-0 shadow-[inset_4px_0_4px_0_rgba(255,255,255,.5)]", 
+						sizing === "sm" && "rounded-sm", 
+						sizing === "md" && "rounded-md", 
+						sizing === "lg" && "rounded-lg"
+					)} />
+					<div className={cn(
+						"absolute inset-0 shadow-[inset_0_4px_4px_0_rgba(255,255,255,.5)]", 
+						sizing === "sm" && "rounded-sm", 
+						sizing === "md" && "rounded-md", 
+						sizing === "lg" && "rounded-lg"
+					)} />
+					<div className={cn(
+						"absolute inset-0 shadow-[inset_0_-4px_4px_0_rgba(255,255,255,.5)]", 
+						sizing === "sm" && "rounded-sm", 
+						sizing === "md" && "rounded-md", 
+						sizing === "lg" && "rounded-lg"
+					)} />
+				</>}
+
 				{children}
 			</button>
 		)
