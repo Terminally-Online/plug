f854a4da376a4c8fda4b6edbc0426e60326d7e5d
Drake Danner <drake.danner@gmail.com>
2024-10-05 17:36:32 -0400

    feat: add pwa.ts

---
+++ lib/pwa.ts
@@ -0,0 +1,11 @@
+export function registerServiceWorker() {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
+}
