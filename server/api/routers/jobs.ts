544801e9e71967718953d84a8b849b0b11ccaae2
nftchance <chance@utc24.io>
2024-10-21 16:27:46 -0500

    chore: cleanup anonymous endpoint

--- server/api/routers/jobs.ts
+++ server/api/routers/jobs.ts
@@ -1,23 +1,13 @@
-// server/api/routers/jobs.ts
-import { TRPCError } from "@trpc/server"
-
-import { z } from "zod"
-
-import { Prisma } from "@prisma/client"
-
 import { apiKeyProcedure, createTRPCRouter } from "../trpc"
 
-// Set the cleanup period to 7 days
 const CLEANUP_OLDER_THAN_DAYS = 7
 
 export const jobs = createTRPCRouter({
 	cleanupAnonymousUsers: apiKeyProcedure.mutation(async ({ ctx }) => {
-		const { db } = ctx
-
 		const cutoffDate = new Date()
 		cutoffDate.setDate(cutoffDate.getDate() - CLEANUP_OLDER_THAN_DAYS)
 
-		const deletedUsers = await db.userSocket.deleteMany({
+		return await ctx.db.userSocket.deleteMany({
 			where: {
 				id: {
 					startsWith: "anonymous-"
@@ -27,11 +17,5 @@ export const jobs = createTRPCRouter({
 				}
 			}
 		})
-
-		return {
-			success: true,
-			deletedUsers: deletedUsers.count,
-			message: `Successfully deleted ${deletedUsers.count} anonymous users older than ${CLEANUP_OLDER_THAN_DAYS} days.`
-		}
 	})
 })
