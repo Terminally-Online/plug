73684a4522fe357c70936fb4168e0289ff37dbb2
nftchance <chance@utc24.io>
2024-10-23 16:53:34 -0500

    chore: cleanup intent package

--- intent/parse.go
+++ intent/parse.go
@@ -1,6 +1,7 @@
 package intent
 
 import (
+	"encoding/json"
 	"fmt"
 	"solver/actions"
 	"solver/types"
@@ -34,6 +35,23 @@ func ParseType(action types.Action) (types.ActionInputs, error) {
 		inputs = &actions.SwapInputsImpl{}
 	default:
 		return nil, fmt.Errorf("unknown action type: %s", action.Type)
+	}
+
+	return inputs, nil
+}
+
+func ParseAction(action types.Action) (types.ActionInputs, error) {
+	inputs, err := ParseType(action)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := json.Unmarshal(action.Inputs, inputs); err != nil {
+		return nil, fmt.Errorf("failed to parse inputs for action %s: %v", action.Type, err)
+	}
+
+	if err := inputs.Validate(); err != nil {
+		return nil, fmt.Errorf("invalid inputs for action %s: %v", action.Type, err)
 	}
 
 	return inputs, nil
