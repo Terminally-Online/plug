137077ee9263312c045879eae0f86ee6b9eece65
Drake Danner <drake.danner@gmail.com>
2024-10-25 18:48:06 -0400

    fix: remove usePlugs import from state/index

--- state/index.ts
+++ state/index.ts
@@ -1,10 +1,6 @@
-import { usePlugs } from "../contexts/PlugProvider"
-
 export * from "./authentication"
 export * from "./columns"
 export * from "./positions"
 export * from "./sidebar"
 export * from "./flags"
 export * from "./recipients"
-
-export { usePlugs }
