e17f749cb0eadfa9596ce8f9ae4826cccee0398e
Drake Danner <drake.danner@gmail.com>
2024-11-07 14:23:31 -0500

    fix: revert to previous add function

--- state/columns.ts
+++ state/columns.ts
@@ -64,31 +64,36 @@ export const useColumns = (index?: number, key?: string) => {
 	 */
 	const add = useCallback(
 		({ index, key, from, item }: Partial<Column>) => {
-		  updateColumns(prev => {
-			const newColumn = {
-			  key,
-			  index: prev.length,
-			  from,
-			  item,
-			  width: DEFAULT_COLUMN_WIDTH
-			} as Column
-			const updatedColumns = [...prev, newColumn]
-	  
-			requestAnimationFrame(() => {
-			  const container = document.querySelector(".flex.h-full.flex-row.overflow-x-auto")
-			  if (container) {
-				container.scrollTo({
-				  left: container.scrollWidth,
-				  behavior: "smooth"
+			updateColumns(prev => {
+				const newColumn = {
+					key,
+					index: index ?? prev.length,
+					from,
+					item,
+					width: DEFAULT_COLUMN_WIDTH
+				} as Column
+				const updatedColumns = [...prev]
+				if (index !== undefined) {
+					updatedColumns.splice(index + COLUMN_OFFSET, 0, newColumn)
+				} else {
+					updatedColumns.push(newColumn)
+				}
+
+				requestAnimationFrame(() => {
+					const container = document.querySelector(".flex.h-full.flex-row.overflow-x-auto")
+					if (container) {
+						container.scrollTo({
+							left: container.scrollWidth,
+							behavior: "smooth"
+						})
+					}
 				})
-			  }
+
+				return updatedColumns.map((col, idx) => ({ ...col, index: idx - COLUMN_OFFSET }))
 			})
-	  
-			return updatedColumns.map((col, idx) => ({ ...col, index: idx - COLUMN_OFFSET }))
-		  })
 		},
 		[updateColumns]
-	  )
+	)
 
 	/**
 	 * Update the state (navigate within the context) of a column.
