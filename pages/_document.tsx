d3aba0212f9ce7ad2aae5223667ae4d0a19f028d
nftchance <chance@utc24.io>
2024-11-13 18:29:01 -0600

    chore: update opengraph metadata and assets

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,8 @@ import { Head, Html, Main, NextScript } from "next/document"
 
 export default function Document() {
 	const title = "Plug"
-	const description = "Designed by humans on Earth. Executed by bots in the cloud."
+	const description =
+		"Designed to let you do more with less. Plug gives you a single interface to manage, compose, schedule, and execute all your transactions and holdings in one place."
 
 	return (
 		<Html lang="en">
