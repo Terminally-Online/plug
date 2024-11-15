a1792a2da67faadb0d5551f5e1e5d8b2bd8b1a83
nftchance <chance@utc24.io>
2024-11-15 10:34:36 -0600

    fix: vision curve not working on all resolutions

--- components/landing/vision.tsx
+++ components/landing/vision.tsx
@@ -91,45 +91,21 @@ export const Vision: FC = () => {
 				</defs>
 			</svg>
 
-			<svg
-				viewBox="0 0 1827 976"
-				fill="none"
+			<svg viewBox="0 0 1827 976" fill="none"
 				className="pointer-events-none absolute inset-0 z-[99] -mr-[10%] -mt-[10%] hidden xl:flex"
 			>
-				<mask
-					id="mask0_4612_101"
-					style={{ maskType: "alpha" }}
-					maskUnits="userSpaceOnUse"
-					x="0"
-					y="59"
-					width="844"
-					height="858"
-				>
-					<path
-						fill-rule="evenodd"
-						clip-rule="evenodd"
-						d="M843.998 59H0V917H370.95V488H642.357V325H843.998V59Z"
-						fill="#D9D9D9"
-					/>
+				<mask id="mask0_4665_134" style={{ maskType: "alpha" }} maskUnits="userSpaceOnUse" x="0" y="67" width="844" height="573">
+					<path fill-rule="evenodd" clip-rule="evenodd" d="M844 67H0V640H370.953V427.587L642.359 427.586V287.976H844V67Z" fill="#D9D9D9" />
 				</mask>
-				<g mask="url(#mask0_4612_101)">
+				<g mask="url(#mask0_4665_134)">
 					<motion.path
-						d="M1798.8 -0.000377674C1476.8 632.5 1272.62 374.395 1151.8 262.984C866.618 -0.000367615 664.618 129.999 556.618 246.499C399.571 415.907 189.936 272.5 74.6184 441.5C-40.6992 610.5 86 932 327 882.5C550.338 836.628 509.184 536.485 886.682 536.485C1167.18 536.485 1126 737 1539.18 657C1699.68 608.5 2001.9 511 1914.3 831"
-						stroke="url(#paint0_linear_4612_101)"
-						stroke-width="60"
 						style={{ pathLength }}
+						d="M1798.8 -0.000377674C1476.8 632.5 1272.62 374.395 1151.8 262.984C866.618 -0.000367615 664.618 129.999 556.618 246.499C399.571 415.907 189.936 272.5 74.6184 441.5C-40.6992 610.5 86 932 327 882.5C550.338 836.628 509.184 536.485 886.682 536.485C1167.18 536.485 1126 737 1539.18 657C1699.68 608.5 2001.9 511 1914.3 831" stroke="url(#paint0_linear_4665_134)" stroke-width="60"
 						strokeLinecap="round"
 					/>
 				</g>
 				<defs>
-					<linearGradient
-						id="paint0_linear_4612_101"
-						x1="567.805"
-						y1="743"
-						x2="1462.3"
-						y2="471.5"
-						gradientUnits="userSpaceOnUse"
-					>
+					<linearGradient id="paint0_linear_4665_134" x1="567.805" y1="743" x2="1462.3" y2="471.5" gradientUnits="userSpaceOnUse">
 						<stop stop-color="#D2F38A" />
 						<stop offset="1" stop-color="#385842" />
 					</linearGradient>
