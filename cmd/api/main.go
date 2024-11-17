359109971e888b9f19b66625cbd83edd0fcf1c88
nftchance <chance@utc24.io>
2024-11-17 16:27:44 -0600

    feat: run cron and api together

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"log"
 	"net/http"
+	"solver/cron"
 	"solver/router"
 	"solver/solver"
 	"solver/utils"
+	"time"
 
 	"github.com/joho/godotenv"
+	scheduler "github.com/robfig/cron"
 )
 
 func main() {
@@ -16,10 +19,25 @@ func main() {
 		log.Fatal(utils.ErrEnvironmentNotInitialized(err.Error()).Error())
 	}
 
-	// Initialize solver with protocols
-	solver := solver.New()
+	location, err := time.LoadLocation("America/New_York")
+	if err != nil {
+		log.Fatal(err)
+	}
+	cronJob := scheduler.NewWithLocation(location)
 
+	for _, job := range cron.CronJobs {
+		err = cronJob.AddFunc(job.Schedule, job.Job)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	log.Printf("Starting %d cron jobs...", len(cron.CronJobs))
+	go cronJob.Start()
+
+	solver := solver.New()
 	router := router.SetupRouter(solver)
+
 	log.Println("Server is running on http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
