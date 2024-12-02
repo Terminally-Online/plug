3e295ad87624a7f571cf83f8c4dd4ce344faf1ed
nftchance <chance@utc24.io>
2024-12-01 21:04:40 -0600

    fix: image urls

--- packages/docs/introduction/IntegrationList.vue
+++ packages/docs/introduction/IntegrationList.vue
@@ -1,13 +1,17 @@
 <template>
   <div :style="styles.integrations">
-    <div 
-      v-for="item in list" 
-      :key="item" 
+    <div
+      v-for="item in list"
+      :key="item"
       :style="styles.integration"
       @mouseenter="onMouseEnter"
       @mouseleave="onMouseLeave"
     >
-      <img :src="getImagePath(item)" :alt="toTitleCase(item)" :style="styles.image">
+      <img
+        :src="getImagePath(item)"
+        :alt="toTitleCase(item)"
+        :style="styles.image"
+      />
       <p :style="styles.title">{{ toTitleCase(item) }}</p>
       <p :style="styles.subtitle">Coming Soon</p>
     </div>
@@ -15,76 +19,76 @@
 </template>
 
 <script setup>
-import { defineProps, reactive } from 'vue'
+import { defineProps, reactive } from "vue";
 
 const props = defineProps({
   list: {
     type: Array,
-    default: () => []
+    default: () => [],
   },
-  type: String
-})
+  type: String,
+});
 
 const styles = reactive({
   integrations: {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(auto-fill, minmax(200px, 1fr))',
-    gap: '20px',
+    display: "grid",
+    gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))",
+    gap: "20px",
   },
   integration: {
-    border: '1px solid rgba(0, 0, 0, 0.1)',
-    padding: '1.5rem',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: '6px',
-    transition: 'background 0.2s',
-    cursor: 'pointer',
+    border: "1px solid rgba(0, 0, 0, 0.1)",
+    padding: "1.5rem",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: "6px",
+    transition: "background 0.2s",
+    cursor: "pointer",
   },
   image: {
-    width: '25%',
-    borderRadius: '50%',
-    marginBottom: '20px',
+    width: "25%",
+    borderRadius: "50%",
+    marginBottom: "20px",
   },
   title: {
-    margin: '0',
-    fontWeight: '700',
+    margin: "0",
+    fontWeight: "700",
   },
   subtitle: {
-    margin: '0',
-    opacity: '0.6',
-    fontSize: '12px',
-    fontWeight: '400',
+    margin: "0",
+    opacity: "0.6",
+    fontSize: "12px",
+    fontWeight: "400",
   },
-})
+});
 
 const onMouseEnter = (event) => {
-  event.currentTarget.style.background = 'rgba(0, 0, 0, 0.04)'
-}
+  event.currentTarget.style.background = "rgba(0, 0, 0, 0.04)";
+};
 
 const onMouseLeave = (event) => {
-  event.currentTarget.style.background = ''
-}
+  event.currentTarget.style.background = "";
+};
 
-const bigList = ["ens"]
+const bigList = ["ens"];
 
-const toChainImagePath = (str) => `/blockchain/${str}.png`
-const toProtocolImagePath = (str) => `/protocols/${str}.png`
-const getImagePath = (str) => props.type === 'chain' 
-  ? toChainImagePath(str) 
-  : toProtocolImagePath(str)
+const toChainImagePath = (str) => `/plug/blockchain/${str}.png`;
+const toProtocolImagePath = (str) => `/plug/protocols/${str}.png`;
+const getImagePath = (str) =>
+  props.type === "chain" ? toChainImagePath(str) : toProtocolImagePath(str);
 
-const toTitleCase = (str) => bigList.includes(str) 
-    ? str.toUpperCase() 
+const toTitleCase = (str) =>
+  bigList.includes(str)
+    ? str.toUpperCase()
     : str
-        .replace(/-/g, ' ')
+        .replace(/-/g, " ")
         .replace(/([a-z])([A-Z])|([A-Z])([A-Z][a-z])/g, "$1$3 $2$4")
         .split(" ")
-        .map(word =>
-            word.toUpperCase() === word && word.length > 1 
-                ? word 
-                : word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+        .map((word) =>
+          word.toUpperCase() === word && word.length > 1
+            ? word
+            : word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
         )
-        .join(" ")
+        .join(" ");
 </script>
