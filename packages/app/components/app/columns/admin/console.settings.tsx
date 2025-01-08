375929c5b82344b0ada4be5f295fcd9b02d22e9e
nftchance <chance@utc24.io>
2025-01-07 20:07:07 -0600

    feat: settings page

--- packages/app/components/app/columns/admin/console.settings.tsx
+++ packages/app/components/app/columns/admin/console.settings.tsx
@@ -1,8 +1,27 @@
 import { FC, HTMLAttributes } from "react"
 
-import { CheckCircle, Clipboard } from "lucide-react"
+import {
+	BookUser,
+	Cable,
+	CalendarCheck,
+	CalendarClock,
+	CheckCircle,
+	Clipboard,
+	Computer,
+	Flower2,
+	Glasses,
+	Globe,
+	Handshake,
+	Hash,
+	MessageCircleIcon,
+	Puzzle,
+	User
+} from "lucide-react"
+
+import plugCore from "@terminallyonline/plug-core/package.json"
 
 import { formatAddress, useClipboard } from "@/lib"
+import app from "@/package.json"
 import { Flag, useFlags, useSocket } from "@/state"
 
 export const ConsoleSettings: FC<HTMLAttributes<HTMLDivElement> & { index: number }> = ({ index, ...props }) => {
@@ -21,8 +40,8 @@ export const ConsoleSettings: FC<HTMLAttributes<HTMLDivElement> & { index: numbe
 				<p className="opacity-40">Socket</p>
 				<div className="h-[2px] w-full bg-plug-green/10" />
 			</div>
-
-			<p className="flex justify-between font-bold">
+			<p className="flex flex-row items-center justify-between gap-2 font-bold">
+				<BookUser size={14} className="opacity-60" />
 				<span className="opacity-40">Address</span>{" "}
 				<span
 					className="group ml-auto flex cursor-pointer flex-row items-center gap-4"
@@ -36,20 +55,20 @@ export const ConsoleSettings: FC<HTMLAttributes<HTMLDivElement> & { index: numbe
 					)}
 				</span>
 			</p>
-
-			<p className="flex flex-row items-center justify-between font-bold">
+			<p className="flex flex-row items-center justify-between gap-2 font-bold">
+				<Hash size={14} className="opacity-60" />
 				<span className="opacity-40">Nonce</span>{" "}
 				<span className="group ml-auto flex flex-row items-center gap-4">1738</span>
 			</p>
-
-			<p className="flex flex-row items-center justify-between font-bold">
+			<p className="flex flex-row items-center justify-between gap-2 font-bold">
+				<Flower2 size={14} className="opacity-60" />
 				<span className="opacity-40">Salt</span>{" "}
 				<span className="group ml-auto flex cursor-pointer flex-row items-center gap-4">
 					{formatAddress(socket?.salt ?? "")}
 				</span>
 			</p>
-
-			<p className="flex justify-between font-bold">
+			<p className="flex flex-row items-center justify-between gap-2 font-bold">
+				<Puzzle size={14} className="opacity-60" />
 				<span className="opacity-40">Implementation</span>{" "}
 				<span className="group ml-auto flex cursor-pointer flex-row items-center gap-4">
 					{socket?.implementation ? formatAddress(socket?.implementation) : "None"}
@@ -60,15 +79,15 @@ export const ConsoleSettings: FC<HTMLAttributes<HTMLDivElement> & { index: numbe
 				<p className="opacity-40">Account</p>
 				<div className="h-[2px] w-full bg-plug-green/10" />
 			</div>
-
-			<p className="flex justify-between font-bold">
+			<p className="flex flex-row items-center justify-between gap-2 font-bold">
+				<CalendarCheck size={14} className="opacity-60" />
 				<span className="opacity-40">Created</span>{" "}
 				<span className="group ml-auto flex flex-row items-center gap-4">
 					{socket?.createdAt.toLocaleDateString()}
 				</span>
 			</p>
-
-			<p className="flex justify-between font-bold">
+			<p className="flex flex-row items-center justify-between gap-2 font-bold">
+				<CalendarClock size={14} className="opacity-60" />
 				<span className="opacity-40">Updated</span>{" "}
 				<span className="group ml-auto flex flex-row items-center gap-4">
 					{socket?.updatedAt.toLocaleDateString()}
@@ -79,21 +98,22 @@ export const ConsoleSettings: FC<HTMLAttributes<HTMLDivElement> & { index: numbe
 				<p className="opacity-40">Identity</p>
 				<div className="h-[2px] w-full bg-plug-green/10" />
 			</div>
-
-			<p className="flex justify-between font-bold">
+			<p className="flex flex-row items-center justify-between gap-2 font-bold">
+				<User size={14} className="opacity-60" />
 				<span className="opacity-40">ENS</span>{" "}
 				<span className="group ml-auto flex flex-row items-center gap-4">
 					{socket?.identity?.ens?.name || "None"}
 				</span>
 			</p>
-
-			<p className="flex justify-between font-bold">
+			<p className="flex flex-row items-center justify-between gap-2 font-bold">
+				<MessageCircleIcon size={14} className="opacity-60" />
 				<span className="opacity-40">Farcaster Id</span>{" "}
 				<span className="group ml-auto flex flex-row items-center gap-4">
 					{socket?.identity?.farcasterId || "None"}
 				</span>
 			</p>
-			<p className="flex justify-between font-bold">
+			<p className="flex flex-row items-center justify-between gap-2 font-bold">
+				<Handshake size={14} className="opacity-60" />
 				<span className="opacity-40">Referred By</span>{" "}
 				<span className="group ml-auto flex flex-row items-center gap-4">
 					{formatAddress(socket?.identity?.referrerId ?? "") || "None"}
@@ -104,11 +124,32 @@ export const ConsoleSettings: FC<HTMLAttributes<HTMLDivElement> & { index: numbe
 				<p className="opacity-40">Flags</p>
 				<div className="h-[2px] w-full bg-plug-green/10" />
 			</div>
-			<p className="flex justify-between font-bold">
+			<p className="flex flex-row items-center justify-between gap-2 font-bold">
+				<Computer size={14} className="opacity-60" />
 				<span className="opacity-40">Show Application</span>{" "}
 				<span className="group ml-auto flex flex-row items-center gap-4">
 					{getFlag(Flag.SHOW_PWA) ? "Yes" : "No"}
 				</span>
+			</p>
+
+			<div className="mt-4 flex flex-row items-center gap-4 font-bold">
+				<p className="opacity-40">Versions</p>
+				<div className="h-[2px] w-full bg-plug-green/10" />
+			</div>
+			<p className="flex flex-row items-center justify-between gap-2 font-bold">
+				<Globe size={14} className="opacity-60" />
+				<span className="opacity-40">App</span>{" "}
+				<span className="group ml-auto flex flex-row items-center gap-4">v{app.version}</span>
+			</p>
+			<p className="flex flex-row items-center justify-between gap-2 font-bold">
+				<Cable size={14} className="opacity-60" />
+				<span className="opacity-40">Cord</span>{" "}
+				<span className="group ml-auto flex flex-row items-center gap-4">v9.1.4</span>
+			</p>
+			<p className="flex flex-row items-center justify-between gap-2 font-bold">
+				<Glasses size={14} className="opacity-60" />
+				<span className="opacity-40">Core</span>{" "}
+				<span className="group ml-auto flex flex-row items-center gap-4">v{plugCore.version}</span>
 			</p>
 		</div>
 	)
