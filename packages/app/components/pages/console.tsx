2d379c16ffbb42670b1ab0caf9735f5a38624cb2
nftchance <chance@utc24.io>
2025-01-10 14:01:45 -0600

    chore: add displayName to console pages

--- packages/app/components/pages/console.tsx
+++ packages/app/components/pages/console.tsx
@@ -1,55 +1,14 @@
 import { signIn, useSession } from "next-auth/react"
 import { useRouter } from "next/router"
-import { memo, useEffect, useRef } from "react"
-
-import { useAccount } from "wagmi"
+import { useEffect, useRef } from "react"
 
 import { LoaderCircle } from "lucide-react"
 
-import { AuthFrame, ConsoleColumnRow, ConsoleSidebar, PageContent, PageNavbar } from "@/components"
-import { LoginRequired } from "@/components/app/utils/login-required"
-import { ReferralRequired } from "@/components/app/utils/referral-required"
-import { useConnect, useMediaQuery } from "@/lib"
-import { useRenderTracking } from "@/lib/hooks/useRenderTracking"
+import { useMediaQuery } from "@/lib"
 import { COLUMNS, useColumnStore, usePlugStore, useSocket, useSubscriptions } from "@/state"
 
-const MobilePage = () => {
-	return (
-		<>
-			<PageContent />
-			<PageNavbar />
-			<AuthFrame />
-		</>
-	)
-}
-
-const DesktopPage = memo(() => {
-	const { data: session } = useSession()
-	const { socket } = useSocket()
-
-	const needsReferral = Boolean(session?.user.id?.startsWith("0x") && socket && !socket.identity?.referrerId)
-	// const renderCount = useRenderTracking("DesktopPage")
-
-	return (
-		<div className="min-w-screen flex h-screen w-full flex-row overflow-y-hidden overflow-x-visible">
-			{/* <div className="border-plug-blue group pointer-events-none absolute left-0 top-0 z-[9999] h-full w-full rounded-lg border-[8px]">
-				<p className="bg-plug-blue absolute bottom-0 w-max rounded-tr-lg px-4 py-2 font-bold text-plug-white">
-					{renderCount.current}
-				</p>
-			</div> */}
-
-			<ConsoleSidebar />
-
-			{!session?.user.id?.startsWith("0x") ? (
-				<LoginRequired />
-			) : needsReferral ? (
-				<ReferralRequired />
-			) : (
-				<ConsoleColumnRow />
-			)}
-		</div>
-	)
-})
+import { DesktopConsole } from "./desktop"
+import { MobileConsole } from "./mobile"
 
 export const ConsolePage = () => {
 	const hasHandledInitialUrl = useRef(false)
@@ -127,7 +86,7 @@ export const ConsolePage = () => {
 	            <DeletedFrame />
 	        */}
 
-			{md ? <DesktopPage /> : <MobilePage />}
+			{md ? <DesktopConsole /> : <MobileConsole />}
 		</>
 	)
 }
