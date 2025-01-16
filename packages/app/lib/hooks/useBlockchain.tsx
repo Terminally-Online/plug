64b49bb6731c0cf13e74a9a23c2245233b9cd030
nftchance <chance@utc24.io>
2025-01-15 22:17:55 -0600

    fix: type and logic errors

--- packages/app/lib/hooks/useBlockchain.tsx
+++ packages/app/lib/hooks/useBlockchain.tsx
@@ -1,12 +1,12 @@
 import { useCallback, useMemo } from "react"
 
-import { formatAddress, getBlockExplorerAddress } from "@/lib"
+import { ChainId, formatAddress, getBlockExplorerAddress } from "@/lib"
 
 export const useBlockchain = ({ address }: { address: string }) => {
 	const displayAddress = useMemo(() => formatAddress(address), [address])
 
 	const blockExplorer = useCallback(
-		(chainId: number) => {
+		(chainId: ChainId) => {
 			getBlockExplorerAddress(chainId, address)
 		},
 		[address]
