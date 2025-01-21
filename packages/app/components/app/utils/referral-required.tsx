72dad0b86bc097ad7882b5accd24e4296dba6d44
Drake Danner <drake.danner@gmail.com>
2025-01-21 18:04:46 -0500

    fix: checkboxes

--- packages/app/components/app/utils/referral-required.tsx
+++ packages/app/components/app/utils/referral-required.tsx
@@ -75,7 +75,7 @@ export const ReferralRequired: FC = () => {
 
 					<div className="w-full space-y-6">
 						{isSuccess && (
-							<div className="w-full rounded-lg border border-plug-yellow-200 bg-plug-yellow-50 p-4 text-sm text-plug-yellow-600">
+							<div className="w-full rounded-lg border border-plug-yellow bg-plug-yellow p-4 text-sm text-plug-yellow">
 								Successfully approved! If you are not redirected in a few seconds, please refresh the
 								page.
 							</div>
