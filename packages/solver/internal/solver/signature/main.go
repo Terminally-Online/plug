6ea40ff5a04cad8434ded08dac5c56f91dc20c27
nftchance <chance@utc24.io>
2025-01-22 13:26:45 -0600

    feat: eip712 message signatures

--- packages/solver/internal/solver/signature/main.go
+++ packages/solver/internal/solver/signature/main.go
@@ -1,8 +1,6 @@
 package signature
 
 import (
-	"encoding/json"
-	"log"
 	"math/big"
 	"os"
 	"solver/internal/utils"
@@ -12,44 +10,72 @@ import (
 )
 
 var (
-	domainName    = "My DApp"
+	domainName    = "Plug"
 	domainVersion = "1"
 )
 
-func GetHash[T any](data T) [32]byte {
-	encoded, err := json.Marshal(data)
-	if err != nil {
-		log.Fatalf("Failed to encode data: %v", err)
+func getPlugHash(plug Plug) [32]byte {
+	encoded := crypto.Keccak256(
+		[]byte(PLUG_TYPEHASH),
+		plug.To.Bytes(),
+		common.LeftPadBytes(plug.Value.Bytes(), 32),
+		crypto.Keccak256(plug.Data),
+	)
+	var hash [32]byte
+	copy(hash[:], encoded)
+	return hash
+}
+
+func getPlugArrayHash(plugs []Plug) [32]byte {
+	var encoded []byte
+	for _, plug := range plugs {
+		hash := getPlugHash(plug)
+		encoded = append(encoded, hash[:]...)
 	}
 
 	return crypto.Keccak256Hash(encoded)
 }
 
-func GetSignature(chainId *big.Int, socket common.Address, plug Plugs) ([]byte, error) {
+func getPlugsHash(plugs Plugs) [32]byte {
+	plugArrayHash := getPlugArrayHash(plugs.Plugs)
+	encoded := crypto.Keccak256(
+		[]byte(PLUGS_TYPEHASH),
+		plugs.Socket.Bytes(),
+		plugArrayHash[:],
+		crypto.Keccak256(plugs.Solver),
+		crypto.Keccak256(plugs.Salt),
+	)
+	var hash [32]byte
+	copy(hash[:], encoded)
+	return hash
+}
+
+func GetSignature(chainId *big.Int, socket common.Address, plugs Plugs) ([]byte, error) {
 	privateKey, err := crypto.HexToECDSA(os.Getenv("SOLVER_PRIVATE_KEY"))
 	if err != nil {
 		return nil, utils.ErrBuild(err.Error())
 	}
 
-	domainHash := GetHash(EIP712Domain{
-		Name:              domainName,
-		Version:           domainVersion,
-		ChainId:           chainId,
-		VerifyingContract: socket,
-	})
-	messageHash := GetHash(Plug{
-		To:    common.HexToAddress("0xRecipientAddress"),
-		Data:  []byte{},
-		Value: big.NewInt(0),
-	})
-	signatureHash := crypto.Keccak256(append(
-		domainHash[:],
-		messageHash[:]...,
-	))
+	domainHash := crypto.Keccak256(
+		[]byte(EIP712_DOMAIN_TYPEHASH),
+		crypto.Keccak256([]byte(domainName)),
+		crypto.Keccak256([]byte(domainVersion)),
+		common.LeftPadBytes(chainId.Bytes(), 32),
+		socket.Bytes(),
+	)
+	plugsHash := getPlugsHash(plugs)
+	signatureHash := crypto.Keccak256(
+		[]byte("\x19\x01"),
+		domainHash,
+		plugsHash[:],
+	)
+
 	signature, err := crypto.Sign(signatureHash, privateKey)
 	if err != nil {
-		log.Fatalf("Failed to sign message: %v", err)
+		return nil, utils.ErrBuild(err.Error())
 	}
+
+	signature[64] += 27
 
 	return signature, nil
 }
--- packages/solver/internal/solver/signature/types.go
+++ packages/solver/internal/solver/signature/types.go
@@ -6,30 +6,11 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 )
 
-var (
-	Eip712Types = map[string]interface{}{
-		"EIP712Domain": []interface{}{
-			map[string]interface{}{"name": "name", "type": "string"},
-			map[string]interface{}{"name": "version", "type": "string"},
-			map[string]interface{}{"name": "chainId", "type": "uint256"},
-			map[string]interface{}{"name": "verifyingContract", "type": "address"},
-		},
-		"Plug": []interface{}{
-			map[string]interface{}{"name": "target", "type": "address"},
-			map[string]interface{}{"name": "value", "type": "uint256"},
-			map[string]interface{}{"name": "data", "type": "bytes"},
-		},
-		"Plugs": []interface{}{
-			map[string]interface{}{"name": "socket", "type": "address"},
-			map[string]interface{}{"name": "plugs", "type": "Plug[]"},
-			map[string]interface{}{"name": "solver", "type": "bytes"},
-			map[string]interface{}{"name": "salt", "type": "bytes"},
-		},
-		"LivePlugs": []interface{}{
-			map[string]interface{}{"name": "plugs", "type": "Plugs"},
-			map[string]interface{}{"name": "signature", "type": "bytes"},
-		},
-	}
+const (
+	EIP712_DOMAIN_TYPEHASH = "0x8b73c3c69bb8fe3d512ecc4cf759cc79239f7b179b0ffacaa9a75d522b39400f"
+	PLUG_TYPEHASH          = "0x0d73e94823fdaacb148d9146f00bc268b7834e768ced483d796db05a52e1e395"
+	PLUGS_TYPEHASH         = "0x4ddfe68cf187b28815da9c19a2cb9477b3f3293c6170c7e3e56842b550ac141d"
+	LIVE_PLUGS_TYPEHASH    = "0x049e34029d287aa78a0f5a45ebdf78081b3357f85f5ab4cfcffb786eff0b3375"
 )
 
 type EIP712Domain struct {
@@ -57,4 +38,9 @@ type Plugs struct {
 type LivePlugs struct {
 	Plugs     Plugs  `json:"plugs"`
 	Signature []byte `json:"signature"`
+}
+
+type Result struct {
+	Success bool   `json:"success"`
+	Result  []byte `json:"result"`
 }
