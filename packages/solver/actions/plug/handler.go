5ff38445b26e23efc8d1b719845855433aaa5fe9
Mason <33527785+masonchain@users.noreply.github.com>
2025-01-22 11:33:00 -0600

    add decimal handling in swaps and transfers

--- packages/solver/actions/plug/handler.go
+++ packages/solver/actions/plug/handler.go
@@ -15,7 +15,7 @@ var (
 
 	schemas = map[types.Action]actions.ActionDefinition{
 		types.ActionTransfer: {
-			Sentence: "Transfer {0<amount:[(1.1)=721?1:uint256]>} {1<token:address:uint256>} {2<id:[(1.1)>20?uint256:null]>} to {3<recipient:address>}",
+			Sentence: "Transfer {0<amount:[(1.1)=721?'1':float]>} {1<token:address:uint256>} {2<id:[(1.1)>20?uint256:null]>} to {3<recipient:address>}",
 			Handler:  HandleTransfer,
 		},
 		types.ActionTransferFrom: {
@@ -23,7 +23,7 @@ var (
 			Handler:  HandleTransferFrom,
 		},
 		types.ActionSwap: {
-			Sentence: "Swap {0<amount:uint256>} {1<tokenIn:address>} for {2<tokenOut:address>}.",
+			Sentence: "Swap {0<amount:float>} {1<tokenIn:address>} for {2<tokenOut:address>}.",
 			Handler:  HandleSwap,
 		},
 	}
