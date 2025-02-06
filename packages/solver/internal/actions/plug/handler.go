bdcb4c69b3b85e4351ea046f9539998230fee0fe
nftchance <chance@utc24.io>
2025-02-06 13:13:39 -0600

    chore: plug action sentence definition

--- packages/solver/internal/actions/plug/handler.go
+++ packages/solver/internal/actions/plug/handler.go
@@ -11,24 +11,23 @@ var (
 	tags = []string{"defi"}
 
 	chains = append(references.Mainnet.ChainIds, references.Base.ChainIds...)
-
 	schemas = map[string]actions.ActionDefinition{
 		actions.ActionTransfer: {
-			Sentence:       "Transfer {0<amount:[(1.1)==721?1:float]>} {1<token:address:uint64:uint64>} {2<id:[(1.1)>20?uint256:null]>} to {3<recipient:address>}",
+			Sentence:       "Transfer {0<amount:[(1.1)==721?1:float]>} {1<token:address:uint64:uint64>} {2<id:[(1.1)>20?uint256:null]>} to {3<recipient:address>}.",
 			Handler:        HandleTransfer,
 			IsUserSpecific: true,
 		},
 		actions.ActionSwap: {
-			Sentence: "Swap {0<amount:float>} {1<tokenIn:address>} for {2<tokenOut:address>}.",
+			Sentence: "Swap {0<amount:float>} {1<tokenIn:address:uint64:uint64>} for {2<tokenOut:address>}.",
 			Handler:  HandleSwap,
 			IsUserSpecific: true,
 		},
 		actions.ConstraintPrice: {
-			Sentence:       "Price of {0<token:address:uint256>} is {1<operator:int8>} than {2<threshold:float>}",
+			Sentence:       "Price of {0<token:address:uint64:uint64>} is {1<operator:int8>} than {2<threshold:float>}.",
 			IsUserSpecific: true,
 		},
 		actions.ConstraintBalance: {
-			Sentence:       "Balance of {0<token:address:uint256>} is {1<operator:int8>} than {2<threshold:float>}",
+			Sentence:       "Balance of {0<token:address:uint64:uint64>} is {1<operator:int8>} than {2<threshold:float>}.",
 			IsUserSpecific: true,
 		},
 	}
