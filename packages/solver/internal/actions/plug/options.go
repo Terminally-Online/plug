04b490b6ccea00e45f8c5558072f5ef9a8ffde9b
nftchance <chance@utc24.io>
2025-02-06 10:15:29 -0600

    feat: zerion functions

--- packages/solver/internal/actions/plug/options.go
+++ packages/solver/internal/actions/plug/options.go
@@ -2,6 +2,7 @@ package plug
 
 import (
 	"solver/internal/actions"
+	// "solver/internal/helpers/zerion"
 
 	"github.com/ethereum/go-ethereum/common"
 )
@@ -9,14 +10,29 @@ import (
 type PlugOptionsProvider struct{}
 
 func (p *PlugOptionsProvider) GetOptions(chainId uint64, from common.Address, action string) (map[int]actions.Options, error) {
+	transferOptions, err := GetTransferOptions(chainId, from)
+	if err != nil {
+		return nil, err
+	}
+
 	switch action {
 	case actions.ActionTransfer:
 		return map[int]actions.Options{
-			1: {Simple: []actions.Option{{
-				Label: from.Hex(),
-			}}},
+			1: {Simple: transferOptions},
 		}, nil
 	default:
 		return nil, nil
 	}
+}
+
+func GetTransferOptions(chainId uint64, from common.Address) ([]actions.Option, error) {
+	// var options []actions.Option
+	//
+	// chainIds := []uint64{chainId}
+	// holdings, err := zerion.GetFungibleHoldings(chainIds, from)
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	return nil, nil
 }
