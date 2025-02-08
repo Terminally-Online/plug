466b36f3f4283628b9881c95d7d72d2f0330efd8
nftchance <chance@utc24.io>
2025-02-08 01:26:38 -0600

    chore: documentation for upcoming issues

--- packages/solver/internal/actions/handler.go
+++ packages/solver/internal/actions/handler.go
@@ -9,8 +9,6 @@ import (
 	"strconv"
 	"strings"
 
-	// "strings"
-
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
@@ -170,6 +168,12 @@ func (h *BaseHandler) GetSchema(chainId string, from common.Address, search map[
 			return nil, fmt.Errorf(errInvalidChainID, chainId)
 		}
 
+		// TODO: (#439) Why is this needed here? Shouldn't this be handled elsewhere? Not sure if it
+		//       actually should be, but we are really far into the compute process to only
+		//       just now be checking this here. Something earlier should have fired this most
+		//       likely like a provider or something. Even if it is not caught earlier, does
+		//       dealing with this here improve things or does it just bloat this function with
+		//       things that we do not actually need?
 		supported := false
 		for _, supportedChain := range h.protocol.Chains {
 			for _, supportedChainId := range supportedChain.ChainIds {
@@ -183,15 +187,38 @@ func (h *BaseHandler) GetSchema(chainId string, from common.Address, search map[
 			return nil, fmt.Errorf("chain not supported: %d", chainIdInt)
 		}
 
+		// NOTE: Override the address value so that we utilize the cache from the global state
+		//       instead of using the "from" parameter as a key lookup even when provided if
+		//       the action being queried against only supports global state.
 		if !h.protocol.Schemas[action].Schema.IsUserSpecific {
 			from = utils.ZeroAddress
 		}
 
+		// TODO: (#473) Due to the current implementation we end up retrieving and calculating the
+		//       options for every action inside of a protocol when we want to retrieve the
+		//       options for a single action. Realistically this should not have a huge
+		//       performance impact however we are going to spend a growing amount of time
+		//       on this and we will pay for a lot of throughput/bandwidth that we do not need.
 		inputs, err := h.protocol.OptionsProvider.GetOptions(chainIdInt, from, search, action)
 		if err != nil {
 			return nil, fmt.Errorf(errFailedOptions, err)
 		}
 
+		// NOTE: This performs really inefficient and really ineffective option searching
+		//       on each index provided a search value. In the current implementation we
+		//       have the base fields searched however searching values beyond that is not
+		//       really scalable at this time.
+		// TODO: (#438) Notably due to the implementation here we end up having a very inefficient
+		//       caching and data retention strategy where we do not reuse the response from
+		//       option providers. Right now, we check cache →  get options →  filter options
+		//       →  cache the response.
+		//       .
+		//       This implementation means that if we have a compute heavy function inside
+		//       of an option we re-run it every time we update the search instead of using
+		//       the cache response and caching that. 
+		//       .
+		//		 To fix this, we would cache the options response and then do live filtering 
+		//       based on the cached options; maybe even caching the then-filtered. 
 		for index := range inputs {
 			if search[index] != "" {
 				temp := inputs[index]
