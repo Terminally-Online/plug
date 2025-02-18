1ef2914d0d4a3795d4b1dcf4ebd26dda3917eba7
nftchance <chance@utc24.io>
2025-02-18 13:33:50 -0600

    feat: functional live plugs preparation in solver

--- packages/solver/internal/solver/main.go
+++ packages/solver/internal/solver/main.go
@@ -3,7 +3,6 @@ package solver
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"math/big"
 	"solver/internal/actions"
 	"solver/internal/actions/aave_v3"
@@ -64,9 +63,9 @@ func (s *Solver) GetTransaction(rawInputs json.RawMessage, chainId uint64, from 
 	}
 
 	params := actions.HandlerParams{
-		Client: client,
-		ChainId:  chainId,
-		From:     from,
+		Client:  client,
+		ChainId: chainId,
+		From:    from,
 	}
 
 	transactions, err := handler.GetTransaction(inputs.Action, rawInputs, params)
@@ -86,74 +85,122 @@ func (s *Solver) GetTransaction(rawInputs json.RawMessage, chainId uint64, from 
 	return transactions, nil
 }
 
+// GetPlugs processes transaction inputs and returns a slice of Plug signatures.
+// It handles both regular and exclusive transactions, where exclusive transactions
+// must be executed independently before other transactions in the batch.
+//
+// Parameters:
+//
+//	head: Existing slice of Plug signatures to append to
+//	chainId: The blockchain network identifier
+//	from: The sender's address
+//	inputs: Raw JSON byte array containing transaction details
+//
+// Returns:
+//
+//	plugs: Slice of Plug signatures (either appended to head or a single exclusive transaction)
+//	exclusive: Boolean indicating if an exclusive transaction was found
+//	error: Any error encountered during processing
+func (s *Solver) GetExclusivePlugs(
+	head []signature.Plug,
+	inputs []byte,
+	chainId uint64,
+	from string,
+) (plugs []signature.Plug, exclusive bool, error error) {
+	plugs, err := s.GetTransaction(inputs, chainId, from)
+	if err != nil {
+		return nil, false, err
+	}
+
+	// NOTE: Some plug actions have exclusive transactions that need to be run alone
+	//       before the rest of the Plug can run. For this, we will just break out
+	//       of the loop and execute any solo transactions that are needed for
+	//       the rest of the batch to run in sequence.
+	for _, plug := range plugs {
+		if plug.Exclusive {
+			// NOTE: Set the field to false to avoid tarnishing the response shape.
+			plug.Exclusive = false
+			return []signature.Plug{plug}, true, nil
+		}
+	}
+
+	return append(head, plugs...), false, nil
+}
+
+// GetPlug processes a simulation definition to generate a sequence of transaction signatures.
+// It iterates through the input definitions, converting them to Plug signatures while handling
+// both regular and exclusive transactions.
+//
+// Parameters:
+//   - definition: A SimulationDefinition containing chain ID, sender address, and transaction inputs
+//
+// Returns:
+//   - []signature.Plug: A slice of transaction signatures ready for execution
+//   - error: Any error encountered during processing, including validation errors
+//
+// The function will return early if it encounters an exclusive transaction, as these must be
+// executed independently. It also validates that at least one transaction is generated.
 func (s *Solver) GetPlugs(definition simulation.SimulationDefinition) ([]signature.Plug, error) {
-	var breakOuter bool
-	transactionsBatch := make([]signature.Plug, 0)
-	errors := make([]error, len(definition.Inputs))
-	for i, input := range definition.Inputs {
-		inputMap := map[string]interface{}{
+	var plugs []signature.Plug
+	for _, input := range definition.Inputs {
+		inputsMap := map[string]interface{}{
 			"protocol": input["protocol"],
 			"action":   input["action"],
 		}
 		for k, v := range input {
-			inputMap[k] = v
+			inputsMap[k] = v
 		}
-
-		inputsJson, err := json.Marshal(inputMap)
-		if err != nil {
-			errors[i] = err
-			continue
-		}
-
-		transactions, err := s.GetTransaction(inputsJson, definition.ChainId, definition.From)
-		if err != nil {
-			errors[i] = err
-			continue
-		}
-
-		log.Println("here in solver main")
-
-		// NOTE: Some plug actions have exclusive transactions that need to be run alone
-		//       before the rest of the Plug can run. For this, we will just break out
-		//       of the loop and execute any solo transactions that are needed for
-		//       the rest of the batch to run in sequence.
-		for _, transaction := range transactions {
-			if transaction.Exclusive {
-				// NOTE: Set the field to false to avoid tarnishing the response shape.
-				transaction.Exclusive = false
-				transactionsBatch = []signature.Plug{transaction}
-				breakOuter = true
-				break
-			}
-		}
-
-		if breakOuter {
-			break
-		}
-
-		transactionsBatch = append(transactionsBatch, transactions...)
-	}
-
-	for _, err := range errors {
+		inputs, err := json.Marshal(inputsMap)
 		if err != nil {
 			return nil, utils.ErrBuild(err.Error())
 		}
+
+		var exclusive bool
+		plugs, exclusive, err = s.GetExclusivePlugs(plugs, inputs, definition.ChainId, definition.From)
+		if err != nil {
+			return nil, utils.ErrBuild(err.Error())
+		}
+		if exclusive {
+			break
+		}
 	}
 
-	// If there was no transaction to execute we will return a warning because
-	// we will be halting the simulation of this workflow.
-	if len(transactionsBatch) == 0 {
+	// NOTE: If there was no transaction to execute we will return a warning because
+	//		 we will be halting the simulation of this workflow.
+	if len(plugs) == 0 {
 		return nil, utils.ErrBuild("no transactions to execute")
 	}
 
-	return transactionsBatch, nil
+	return plugs, nil
 }
 
-func (s *Solver) GetPlugsTransaction() error {
-	return nil
+// GetLivePlugs converts a simulation definition into executable transaction signatures.
+// It first generates Plug signatures using GetPlugs, then converts them into LivePlugs
+// which contain additional metadata needed for transaction execution.
+//
+// Parameters:
+//   - definition: A SimulationDefinition containing chain ID, sender address, and transaction inputs
+//
+// Returns:
+//   - signature.LivePlugs: A struct containing the executable transaction data
+//   - error: Any error encountered during the conversion process
+func (s *Solver) GetLivePlugs(definition simulation.SimulationDefinition) (signature.LivePlugs, error) {
+	plugs, err := s.GetPlugs(definition)
+	if err != nil {
+		return signature.LivePlugs{}, err
+	}
+	livePlugs, err := signature.GetLivePlugs(definition.ChainId, definition.From, plugs)
+	if err != nil {
+		return signature.LivePlugs{}, err
+	}
+
+	return livePlugs, nil
 }
 
-func (s *Solver) GetCall(transactions []signature.Plug) error {
-	// TODO: Run the transactions through the entrypoint with our executor account.
-	return nil
-}
+// _, simulationResponse, err := s.Simulator.GetSimulationResponse(definition.Id, definition.ChainId, livePlugs)
+// if err != nil {
+// 	simulationResponses = append(simulationResponses, simulation.SimulationResponse{
+// 		Success: false,
+// 	})
+// 	continue
+// }
