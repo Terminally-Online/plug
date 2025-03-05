b89fda64604c9c0a87443fc6e03bf3e79a4f3fb4
Mason <33527785+masonchain@users.noreply.github.com>
2025-03-05 13:31:33 -0600

    fix: solver db using utc

--- packages/solver/internal/database/database.go
+++ packages/solver/internal/database/database.go
@@ -59,6 +59,9 @@ func Connect(config *Config) (*gorm.DB, error) {
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
 		Logger: newLogger,
+		NowFunc: func() time.Time {
+			return time.Now().UTC()
+		},
 	})
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to database: %w", err)
