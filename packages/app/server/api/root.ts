b0cdc3e341395c40d226cb64c2a58809ffb02031
nftchance <chance@utc24.io>
2025-03-09 16:39:23 -0500

    chore: replace cron job endpoints with internal scripts

--- packages/app/server/api/root.ts
+++ packages/app/server/api/root.ts
@@ -1,6 +1,5 @@
 import { inferRouterInputs, inferRouterOutputs } from "@trpc/server"
 
-import { jobs } from "@/server/api/routers/jobs"
 import { misc } from "@/server/api/routers/misc"
 import { plugs } from "@/server/api/routers/plugs"
 import { socket } from "@/server/api/routers/socket"
@@ -10,7 +9,6 @@ import { createTRPCRouter } from "@/server/api/trpc"
 export const appRouter = createTRPCRouter({
 	plugs,
 	socket,
-	jobs,
 	misc,
 	solver
 })
