92aa2a76adf02795b1c2dad20ee79681f244990e
Mason <33527785+masonchain@users.noreply.github.com>
2025-04-04 12:08:47 -0500

    fix: math linked inputs

--- packages/solver/internal/actions/math/actions/power.go
+++ packages/solver/internal/actions/math/actions/power.go
@@ -6,39 +6,56 @@ import (
 	"solver/bindings/plug_math"
 	"solver/internal/actions"
 	"solver/internal/bindings/references"
+	"solver/internal/coil"
 	"solver/internal/solver/signature"
 
 	"github.com/ethereum/go-ethereum/common"
 )
 
 type PowerRequest struct {
-	Base     *big.Int `json:"base"`
-	Exponent *big.Int `json:"exponent"`
+	Base     coil.CoilInput[*big.Int, *big.Int] `json:"base"`
+	Exponent *big.Int                           `json:"exponent"`
+}
+
+var PowerFunc = actions.ActionOnchainFunctionResponse{
+	Metadata:     plug_math.PlugMathMetaData,
+	FunctionName: "power",
 }
 
 func Power(lookup *actions.SchemaLookup[PowerRequest]) ([]signature.Plug, error) {
-	if lookup.Inputs.Base == nil || lookup.Inputs.Exponent == nil {
+	if lookup.Inputs.Base.GetValue() == nil || lookup.Inputs.Exponent == nil {
 		return nil, fmt.Errorf("base and exponent values must be provided")
 	}
 
-	mathContract := common.HexToAddress(references.Networks[lookup.ChainId].References["plug"]["math"])
-	mathAbi, err := plug_math.PlugMathMetaData.GetAbi()
+	var baseUpdates []coil.Update
+	base, baseUpdates, err := actions.GetAndUpdate(
+		&lookup.Inputs.Base,
+		lookup.Inputs.Base.GetValueWithError,
+		&PowerFunc,
+		"base",
+		baseUpdates,
+		lookup.PreviousActionDefinition,
+	)
 	if err != nil {
-		return nil, fmt.Errorf("failed to get PlugMath ABI: %w", err)
+		return nil, fmt.Errorf("failed to get base value: %w", err)
 	}
 
 	if lookup.Inputs.Exponent.Cmp(big.NewInt(1000)) > 0 {
 		return nil, fmt.Errorf("exponent too large: maximum allowed is 1000")
 	}
 
-	calldata, err := mathAbi.Pack("power", lookup.Inputs.Base, lookup.Inputs.Exponent)
+	calldata, err := PowerFunc.GetCalldata(
+		base,
+		lookup.Inputs.Exponent,
+	)
 	if err != nil {
-		return nil, fmt.Errorf("failed to pack power calldata: %w", err)
+		return nil, fmt.Errorf("failed to get calldata: %w", err)
 	}
 
 	return []signature.Plug{{
-		To:    mathContract,
-		Data:  calldata,
-		Value: nil,
+		To:      common.HexToAddress(references.Networks[lookup.ChainId].References["plug"]["math"]),
+		Data:    calldata,
+		Value:   nil,
+		Updates: baseUpdates,
 	}}, nil
 }
