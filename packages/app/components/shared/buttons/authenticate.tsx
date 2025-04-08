1dfc600c18d7174e5933eb5a05b51cef71095b46
nftchance <chance@utc24.io>
2025-04-07 23:11:03 -0500

    chore: chain → chainId for ChainSpecificButton

--- packages/app/components/shared/buttons/authenticate.tsx
+++ packages/app/components/shared/buttons/authenticate.tsx
@@ -1,23 +1,23 @@
 import { FC } from "react"
 
-import { Chain } from "viem"
 import { useSwitchChain } from "wagmi"
 
 import { Button } from "./button"
 import { useAccount } from "@/lib/hooks/account/useAccount"
+import { ChainId } from "@/lib"
 
-type Props = Parameters<typeof Button>[0] & { chain: Chain }
+type Props = Parameters<typeof Button>[0] & { chainId: ChainId }
 
-export const ChainSpecificButton: FC<Props> = ({ chain, ...rest }) => {
-	const { chainId } = useAccount()
+export const ChainSpecificButton: FC<Props> = ({ chainId, ...rest }) => {
+	const account = useAccount()
 
 	const { switchChainAsync } = useSwitchChain()
 
 	const handleSwitch = async () => {
-		await switchChainAsync({ chainId: chain.id })
+		await switchChainAsync({ chainId: chainId })
 	}
 
-	if (chainId !== chain.id)
+	if (chainId !== account.chainId)
 		return (
 			<Button {...rest} href={undefined} onClick={handleSwitch}>
 				Switch Chain
