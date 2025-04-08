24a2b93db6c1bdb0a34be3a7919c63ae5debf081
nftchance <chance@utc24.io>
2025-04-07 23:10:27 -0500

    feat: manual socket deployment per chain

--- packages/app/components/app/frames/assets/scrolling-error.tsx
+++ packages/app/components/app/frames/assets/scrolling-error.tsx
@@ -1,13 +1,13 @@
 import { Marquee } from "../../utils/marquee"
 
-export const ScrollingError = ({ error }: { error: string | undefined }) => {
+export const ScrollingError = ({ error, duration }: { error: string | undefined, duration?: number }) => {
 	if (!error) return null
 
 	return (
 		<div className="relative min-h-6 overflow-x-hidden">
 			<div className="absolute bottom-0 left-0 top-0 z-[20] w-12 bg-gradient-to-r from-plug-white to-plug-white/0" />
 			<div className="absolute bottom-0 right-0 top-0 z-[20] w-12 bg-gradient-to-l from-plug-white to-plug-white/0" />
-			<Marquee className="-z-1 relative max-w-full whitespace-nowrap font-bold text-plug-red">{error}</Marquee>
+			<Marquee duration={duration} className="-z-1 relative max-w-full whitespace-nowrap font-bold text-plug-red">{error}</Marquee>
 		</div>
 	)
 }
---
+++ packages/app/lib/hooks/socket/useBytecode.ts
@@ -0,0 +1,37 @@
+import { useEffect, useMemo, useState } from 'react'
+import { createClient } from '@/lib'
+import { mainnet } from 'viem/chains'
+import { GetCodeReturnType } from 'viem'
+
+export const useBytecode = (address: string) => {
+	const [bytecode, setBytecode] = useState<GetCodeReturnType | null>(null)
+	const [isLoading, setIsLoading] = useState(false)
+	const [error, setError] = useState<Error | null>(null)
+
+	const client = useMemo(() => createClient(mainnet.id), [])
+
+	useEffect(() => {
+		if (!address) return
+
+		const getCode = async () => {
+			try {
+				setIsLoading(true)
+				setError(null)
+				const code = await client.getCode({
+					address: address as `0x${string}`,
+				})
+				setBytecode(code)
+			} catch (err) {
+				console.error("Error fetching contract bytecode", err)
+				setError(err instanceof Error ? err : new Error(String(err)))
+			} finally {
+				setIsLoading(false)
+			}
+		}
+
+		getCode()
+	}, [address])
+
+	return { bytecode, error, isLoading, isError: error !== null }
+
+}
