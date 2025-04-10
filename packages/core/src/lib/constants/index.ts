aa4aa111ab669a1a8a06c77992d1042eb957e104
nftchance <chance@utc24.io>
2025-04-09 22:17:53 -0500

    chore: deployment script generation for action contracts

--- packages/core/src/lib/constants/index.ts
+++ packages/core/src/lib/constants/index.ts
@@ -10,6 +10,11 @@ const base = (name: string): Contract => ({
 	relativePath: '../base/'
 })
 
+const actions = (name: string): Contract => ({
+	name,
+	relativePath: '../actions/'
+})
+
 export const router = base('Plug.sol')
 export const factory = base('Plug.Factory.sol')
 export const socket = base('Plug.Socket.sol')
@@ -17,15 +22,17 @@ export const ticket = base('Plug.Ticket.sol')
 export const token = base('Plug.Token.sol')
 export const rewards = base('Plug.Rewards.sol')
 
-export const assert = base('Plug.Assert.sol')
-export const boolean = base('Plug.Boolean.sol')
-export const coercion = base('Plug.Coercion.sol')
-export const database = base('Plug.Database.sol')
-export const evm = base('Plug.EVM.sol')
-export const math = base('Plug.Math.sol')
+export const assert = actions('Plug.Assert.sol')
+export const boolean = actions('Plug.Boolean.sol')
+export const coercion = actions('Plug.Coercion.sol')
+export const database = actions('Plug.Database.sol')
+export const evm = actions('Plug.EVM.sol')
+export const math = actions('Plug.Math.sol')
 
 export const constantContracts: Readonly<Array<Contract>> = [
+	router,
 	factory,
+	socket,
 	ticket,
 	assert,
 	boolean,
@@ -39,5 +46,14 @@ export const constantContracts: Readonly<Array<Contract>> = [
 export const etchContracts: Readonly<Array<Contract>> = [
 	router,
 	factory,
-	socket
+	socket,
+	ticket,
+	assert,
+	boolean,
+	coercion,
+	database,
+	evm,
+	math,
+	token,
+	rewards
 ] as const
--- packages/core/src/lib/functions/bundle/addresses.ts
+++ packages/core/src/lib/functions/bundle/addresses.ts
@@ -1,30 +1,32 @@
 import addresses from '../../addresses.json'
 import { version } from 'package.json'
-import { encodeAbiParameters, getContractAddress, Hex, parseAbiParameters } from 'viem'
+import {
+	encodeAbiParameters,
+	getContractAddress,
+	Hex,
+	parseAbiParameters
+} from 'viem'
 
 export const getSocketImplementation = () => {
-    const versioned = addresses[version as keyof typeof addresses]
-    return versioned.contracts['Plug.Socket.sol']
+	const versioned = addresses[version as keyof typeof addresses]
+	return versioned.contracts['Plug.Socket.sol']
 }
 
-export const getSocketSalt = (
-    nonce: bigint,
-    admin: `0x${string}`,
-) => {
-    return encodeAbiParameters(
-        parseAbiParameters(["uint96", "address"]),
-        [nonce, admin],
-    )
+export const getSocketSalt = (nonce: bigint, admin: `0x${string}`) => {
+	return encodeAbiParameters(parseAbiParameters(['uint96', 'address']), [
+		nonce,
+		admin
+	])
 }
 
 export const getSocketAddress = (salt: Hex) => {
-    const socketImplementation = getSocketImplementation()
-    const address = getContractAddress({
-        bytecodeHash: socketImplementation.initCodeHash as `0x${string}`,
-        from: socketImplementation.deployment.address as `0x${string}`,
-        opcode: 'CREATE2',
-        salt
-    })
+	const socketImplementation = getSocketImplementation()
+	const address = getContractAddress({
+		bytecodeHash: socketImplementation.initCodeHash as `0x${string}`,
+		from: socketImplementation.deployment.address as `0x${string}`,
+		opcode: 'CREATE2',
+		salt
+	})
 
-    return { address }
+	return { address }
 }
