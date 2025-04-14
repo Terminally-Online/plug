1796addfcf588e561eb66f3ada0a4a1008fbfd4b
nftchance <chance@utc24.io>
2025-04-14 14:11:57 -0500

    chore: functional cleanup

--- packages/core/src/contracts/base/Plug.Socket.sol
+++ packages/core/src/contracts/base/Plug.Socket.sol
@@ -2,16 +2,17 @@
 
 pragma solidity ^0.8.26;
 
-import {PlugSocketInterface} from '../interfaces/Plug.Socket.Interface.sol';
-import {PlugTypes} from '../abstracts/Plug.Types.sol';
 import {Ownable} from 'solady/auth/Ownable.sol';
 import {Receiver} from 'solady/accounts/Receiver.sol';
 import {UUPSUpgradeable} from 'solady/utils/UUPSUpgradeable.sol';
 import {ReentrancyGuard} from 'solady/utils/ReentrancyGuard.sol';
 import {LibBitmap} from 'solady/utils/LibBitmap.sol';
 import {ECDSA} from 'solady/utils/ECDSA.sol';
-import {PlugLib, PlugTypesLib} from '../libraries/Plug.Lib.sol';
 import {LibBytes} from 'solady/utils/LibBytes.sol';
+
+import {PlugSocketInterface} from '../interfaces/Plug.Socket.Interface.sol';
+import {PlugTypes} from '../abstracts/Plug.Types.sol';
+import {PlugLib, PlugTypesLib} from '../libraries/Plug.Lib.sol';
 
 /**
  * @title PlugSocket
@@ -46,8 +47,6 @@ contract PlugSocket is
 
 	/**
 	 * @notice Modifier to enforce the signer of the transaction.
-	 * @dev Apply this to functions that are designed to execute a bundle
-	 *      of Plugs regardless of whether through a Router or or direct access.
 	 * @param $input The LivePlugs the definition of execution as well as the
 	 *               signature used to verify the execution permission.
 	 */
@@ -63,8 +62,6 @@ contract PlugSocket is
 
 	/**
 	 * @notice Modifier to enforce the sender of the transaction.
-	 * @dev Apply this to functions that are designed to execute a bundle
-	 *      of Plugs directly from the sender or a recursive call from the contract.
 	 */
 	modifier enforceSender() {
 		if (_enforceSender(msg.sender) == false) {
@@ -112,7 +109,7 @@ contract PlugSocket is
 	}
 
 	/**
-	 * See {PlugSocketInterface-initialize}.
+	 * @notice See {PlugSocketInterface-initialize}.
 	 */
 	function initialize(address $owner, address $oneClicker) public {
 		_initializeOwner($owner);
@@ -123,36 +120,22 @@ contract PlugSocket is
 	}
 
 	/**
-	 * See {PlugSocketInterface-plug}.
+	 * @notice See {PlugSocketInterface-plug}.
 	 */
 	function plug(
 		PlugTypesLib.LivePlugs calldata $livePlugs,
 		address $solver
-	)
-		external
-		payable
-		virtual
-		nonReentrant
-		enforceSignature($livePlugs)
-		returns (PlugTypesLib.Result memory $results)
-	{
-		$results = _plug($livePlugs.plugs, $solver);
+	) external payable virtual nonReentrant enforceSignature($livePlugs) {
+		_plug($livePlugs.plugs, $solver);
 	}
 
 	/**
-	 * See {PlugSocketInterface-plug}.
+	 * @notice See {PlugSocketInterface-plug}.
 	 */
 	function plug(
 		PlugTypesLib.Plugs calldata $plugs
-	)
-		external
-		payable
-		virtual
-		nonReentrant
-		enforceSender
-		returns (PlugTypesLib.Result memory $results)
-	{
-		$results = _plug($plugs, address(0));
+	) external payable virtual nonReentrant enforceSender {
+		_plug($plugs, address(0));
 	}
 
 	/**
@@ -170,21 +153,21 @@ contract PlugSocket is
 	}
 
 	/**
-	 * See { PlugSocket-name }
+	 * @notice See { PlugSocket-name }
 	 */
 	function name() public pure override returns (string memory $name) {
 		$name = 'Plug Socket';
 	}
 
 	/**
-	 * See { PlugSocket-version }
+	 * @notice See { PlugSocket-version }
 	 */
 	function version() public pure override returns (string memory $version) {
 		$version = '0.0.1';
 	}
 
 	/**
-	 * See { PlugSocket-hash }
+	 * @notice See { PlugSocket-hash }
 	 */
 	function hash(
 		PlugTypesLib.LivePlugs calldata $livePlugs
@@ -193,17 +176,50 @@ contract PlugSocket is
 	}
 
 	/**
-	 * @notice Build the transaction data for the next action using the provided
-	 *         coil update instructions.
-	 * @dev This function is the only function in the entire protocol that is not
-	 *      easily understood without a significant amount of EVM knowledge and
-	 *      protocol context. If you are a developer reading this, please reach out
-	 *      to us if you have any questions about what is going on here.
-	 * @param $i The index of the Plug in the Plugs array.
-	 * @param $update The coil update instructions.
-	 * @param $data The transaction data.
-	 * @param $coil The coil data.
-	 * @return $data The updated transaction data.
+	 * @notice Confirm that signer has permission to declare execution of a
+	 *         Plug bundle on the parent-socket that inherits this contract.
+	 * @dev Inheriting contracts must implement the logic of this function to make
+	 *      sure that only signatures intended for this scope are allowed.
+	 * @param $input The LivePlugs object that contains the Plugs object as well as
+	 *               the signature defining the permission to execute the bundle.
+	 * @return $allowed True if the signature is valid, false otherwise.
+	 */
+	function _enforceSignature(
+		PlugTypesLib.LivePlugs calldata $input
+	) internal virtual returns (bool $allowed) {
+		address signer = getPlugsDigest($input.plugs).recover($input.signature);
+		uint256 nonce = uint256(uint96(bytes12($input.plugs.salt)));
+		if (nonces.get(nonce) == true) {
+			revert PlugLib.PlugFailed(
+				type(uint8).max,
+				'PlugCore:nonce-invalid'
+			);
+		}
+		nonces.set(nonce);
+		$allowed = oneClickersToAllowed[signer] || owner() == signer;
+	}
+
+	/**
+	 * @notice Confirm that the sender of the transaction is allowed as the Socket
+	 *         was directly interacted with.
+	 * @dev Inheriting contracts must implement the logic of this function to make
+	 *      sure that only senders intended for this scope are allowed.
+	 * @param $sender The sender of the transaction.
+	 * @return $allowed True if the sender is allowed, false otherwise.
+	 */
+	function _enforceSender(
+		address $sender
+	) internal view virtual returns (bool $allowed) {
+		$allowed = $sender == owner() || $sender == address(this);
+	}
+
+	/**
+	 * @notice Coil the data for the given update.
+	 * @param $i The index of the Plug.
+	 * @param $update The update to coil the data for.
+	 * @param $data The data to coil the data for.
+	 * @param $coil The coil to coil the data for.
+	 * @return $charge The charged data.
 	 */
 	function _coil(
 		uint256 $i,
@@ -311,16 +327,11 @@ contract PlugSocket is
 	 * @notice Execute a set of Plugs.
 	 * @param $plugs The Plugs to execute containing the bundle and side effects.
 	 * @param $solver Encoded data defining the Solver and compensation.
-	 * @return $results The results of the execution.
 	 */
 	function _plug(
 		PlugTypesLib.Plugs calldata $plugs,
 		address $solver
-	)
-		internal
-		enforceSolver($plugs.solver, $solver)
-		returns (PlugTypesLib.Result memory $results)
-	{
+	) internal enforceSolver($plugs.solver, $solver) {
 		uint256 length = $plugs.plugs.length;
 		bool success;
 		bytes[] memory inputs = new bytes[](length);
@@ -345,62 +356,22 @@ contract PlugSocket is
 			(success, results[i]) = _call($plugs.plugs[i], inputs[i]);
 			if (!success) revert PlugLib.PlugFailed(i, 'PlugCore:plug-failed');
 		}
-
-		$results = PlugTypesLib.Result({index: type(uint8).max, error: ''});
 	}
 
 	/**
-	 * @notice Confirm that signer has permission to declare execution of a
-	 *         Plug bundle on the parent-socket that inherits this contract.
-	 * @dev Inheriting contracts must implement the logic of this function to make
-	 *      sure that only signatures intended for this scope are allowed.
-	 * @param $input The LivePlugs object that contains the Plugs object as well as
-	 *               the signature defining the permission to execute the bundle.
-	 * @return $allowed True if the signature is valid, false otherwise.
+	 * @notice See { Ownable._guardInitializeOwner }
 	 */
-	function _enforceSignature(
-		PlugTypesLib.LivePlugs calldata $input
-	) internal virtual returns (bool $allowed) {
-		address signer = getPlugsDigest($input.plugs).recover($input.signature);
-		uint256 nonce = uint256(uint96(bytes12($input.plugs.salt)));
-		if (nonces.get(nonce) == true) {
-			revert PlugLib.PlugFailed(
-				type(uint8).max,
-				'PlugCore:nonce-invalid'
-			);
-		}
-		nonces.set(nonce);
-		$allowed = oneClickersToAllowed[signer] || owner() == signer;
-	}
-
-	/**
-	 * @notice Confirm that the sender of the transaction is allowed as the Socket
-	 *         was directly interacted with.
-	 * @dev Inheriting contracts must implement the logic of this function to make
-	 *      sure that only senders intended for this scope are allowed.
-	 * @param $sender The sender of the transaction.
-	 * @return $allowed True if the sender is allowed, false otherwise.
-	 */
-	function _enforceSender(
-		address $sender
-	) internal view virtual returns (bool $allowed) {
-		$allowed = $sender == owner() || $sender == address(this);
-	}
-
-	/**
-	 * See { PlugTrading._guardInitializeOwnership }
-	 */
-	function _guardInitializeOwnership()
+	function _guardInitializeOwner()
 		internal
 		pure
-		virtual
+		override
 		returns (bool $guard)
 	{
 		$guard = true;
 	}
 
 	/**
-	 * See { UUPSUpgradeable._authorizeUpgrade }
+	 * @notice See { UUPSUpgradeable._authorizeUpgrade }
 	 */
 	function _authorizeUpgrade(address) internal virtual override onlyOwner {}
 }
--- packages/core/src/contracts/interfaces/Plug.Socket.Interface.sol
+++ packages/core/src/contracts/interfaces/Plug.Socket.Interface.sol
@@ -3,49 +3,42 @@
 pragma solidity ^0.8.26;
 
 /// @dev Shape declarations in the Plug framework.
-import { PlugTypesLib } from "../abstracts/Plug.Types.sol";
+import {PlugTypesLib} from '../abstracts/Plug.Types.sol';
 
 interface PlugSocketInterface {
-    /**
-     * @notice Initialize the Socket with the ownership proxy of the Socket.
-     * @param $owner The address of the owner.
-     * @param $oneClicker The address of the one clicker.
-     */
-    function initialize(address $owner, address $oneClicker) external;
+	/**
+	 * @notice Initialize the Socket with the ownership proxy of the Socket.
+	 * @param $owner The address of the owner.
+	 * @param $oneClicker The address of the one clicker.
+	 */
+	function initialize(address $owner, address $oneClicker) external;
 
-    /**
-     * @notice Returns the hash expected from an intent ran through this Socket.
-     * @param $livePlugs The Plug bundle to get the hash for.
-     * @return $livePlugsHash The hash of the signed contents.
-     */
-    function hash(PlugTypesLib.LivePlugs calldata $livePlugs)
-        external
-        view
-        returns (bytes32 $livePlugsHash);
+	/**
+	 * @notice Returns the hash expected from an intent ran through this Socket.
+	 * @param $livePlugs The Plug bundle to get the hash for.
+	 * @return $livePlugsHash The hash of the signed contents.
+	 */
+	function hash(
+		PlugTypesLib.LivePlugs calldata $livePlugs
+	) external view returns (bytes32 $livePlugsHash);
 
-    /**
-     * @notice Allows anyone to submit a plugs of signed plugs for processing.
-     * @notice This version of the function will always be called by the Router.
-     * @param $livePlugs The Plug bundle to execute.
-     * @param $solver The address of the Solver.
-     * @return $results The return data of each plug executed.
-     */
-    function plug(
-        PlugTypesLib.LivePlugs calldata $livePlugs,
-        address $solver
-    )
-        external
-        payable
-        returns (PlugTypesLib.Result memory $results);
+	/**
+	 * @notice Allows anyone to submit a plugs of signed plugs for processing.
+	 * @notice This version of the function will always be called by the Router.
+	 * @param $livePlugs The Plug bundle to execute.
+	 * @param $solver The address of the Solver.
+	 */
+	function plug(
+		PlugTypesLib.LivePlugs calldata $livePlugs,
+		address $solver
+	) external payable;
 
-    /**
-     * @notice Allows anyone to submit a plugs of signed plugs for processing.
-     * @notice This version of the function will always be called by the Router.
-     * @param $plugs The Plug bundle to execute.
-     * @return $results The return data of each plug executed.
-     */
-    function plug(PlugTypesLib.Plugs calldata $plugs)
-        external
-        payable
-        returns (PlugTypesLib.Result memory $results);
+	/**
+	 * @notice Allows anyone to submit a plugs of signed plugs for processing.
+	 * @notice This version of the function will always be called by the Router.
+	 * @param $plugs The Plug bundle to execute.
+	 */
+	function plug(
+		PlugTypesLib.Plugs calldata $plugs
+	) external payable;
 }
