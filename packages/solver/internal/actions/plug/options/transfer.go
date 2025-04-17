8816bb7250eef0415d3584beb979351b619d60ac
Mason <33527785+masonchain@users.noreply.github.com>
2025-04-17 11:47:26 -0500

    fix: all erc transfer support

--- packages/solver/internal/actions/plug/options/transfer.go
+++ packages/solver/internal/actions/plug/options/transfer.go
@@ -1,16 +1,41 @@
 package options
 
 import (
+	"slices"
 	"solver/internal/actions"
 	"solver/internal/actions/options"
 )
 
 func TransferOptions[T any](lookup *actions.SchemaLookup[T]) (map[int]actions.Options, error) {
-	fungiblesIndex := 1
-	fungiblesOptions, err := options.GetFungiblesAndFungiblesHeldOptions(lookup, fungiblesIndex)
+	holdingsIndex := 1
+	heldFungiblesOptions, err := options.GetFungiblesHeldOptions(lookup, holdingsIndex)
 	if err != nil {
 		return nil, err
 	}
+
+	otherFungiblesOptions, err := options.GetFungiblesOptions(lookup, holdingsIndex)
+	if err != nil {
+		return nil, err
+	}
+
+	heldFungiblesMap := make(map[string]actions.Option, len(heldFungiblesOptions))
+	for _, option := range heldFungiblesOptions {
+		heldFungiblesMap[option.Value] = option
+	}
+
+	// TODO MASON: what search index should I be using?
+	heldNonFungiblesOptions, err := options.GetCollectiblesHeldOptions(lookup, holdingsIndex)
+	if err != nil {
+		return nil, err
+	}
+
+	holdingsOptions := append(heldFungiblesOptions, heldNonFungiblesOptions...)
+	allHoldingsOptions := make([]actions.Option, 0, len(holdingsOptions)+len(otherFungiblesOptions))
+	allHoldingsOptions = append(allHoldingsOptions, holdingsOptions...)
+	allHoldingsOptions = append(allHoldingsOptions, slices.DeleteFunc(otherFungiblesOptions, func(o actions.Option) bool {
+		_, exists := heldFungiblesMap[o.Value]
+		return exists
+	})...)
 
 	recipientIndex := 2
 	recipientOptions, err := options.GetAddressOptions(lookup, recipientIndex)
@@ -19,7 +44,7 @@ func TransferOptions[T any](lookup *actions.SchemaLookup[T]) (map[int]actions.Op
 	}
 
 	return map[int]actions.Options{
-		fungiblesIndex: {Simple: fungiblesOptions},
+		holdingsIndex:  {Simple: allHoldingsOptions},
 		recipientIndex: {Simple: recipientOptions},
 	}, nil
 }
