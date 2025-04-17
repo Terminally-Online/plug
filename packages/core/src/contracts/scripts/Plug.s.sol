76da01ea11278db13f3e33561282c81f9d42436d
nftchance <chance@utc24.io>
2025-04-17 15:21:25 -0500

    chore: rebuild post force-push

--- packages/core/src/contracts/scripts/Plug.s.sol
+++ packages/core/src/contracts/scripts/Plug.s.sol
@@ -18,25 +18,29 @@ contract PlugDeployment is Script {
 
         if (PlugEtcherLib.PLUG_ASSERT_ADDRESS.code.length == 0) {
             PlugEtcherLib.FACTORY.safeCreate2(
-                PlugEtcherLib.PLUG_ASSERT_SALT, PlugEtcherLib.PLUG_ASSERT_INITCODE
+                PlugEtcherLib.PLUG_ASSERT_SALT,
+                PlugEtcherLib.PLUG_ASSERT_INITCODE
             );
         }
 
         if (PlugEtcherLib.PLUG_BOOLEAN_ADDRESS.code.length == 0) {
             PlugEtcherLib.FACTORY.safeCreate2(
-                PlugEtcherLib.PLUG_BOOLEAN_SALT, PlugEtcherLib.PLUG_BOOLEAN_INITCODE
+                PlugEtcherLib.PLUG_BOOLEAN_SALT,
+                PlugEtcherLib.PLUG_BOOLEAN_INITCODE
             );
         }
 
         if (PlugEtcherLib.PLUG_COERCION_ADDRESS.code.length == 0) {
             PlugEtcherLib.FACTORY.safeCreate2(
-                PlugEtcherLib.PLUG_COERCION_SALT, PlugEtcherLib.PLUG_COERCION_INITCODE
+                PlugEtcherLib.PLUG_COERCION_SALT,
+                PlugEtcherLib.PLUG_COERCION_INITCODE
             );
         }
 
         if (PlugEtcherLib.PLUG_DATABASE_ADDRESS.code.length == 0) {
             PlugEtcherLib.FACTORY.safeCreate2(
-                PlugEtcherLib.PLUG_DATABASE_SALT, PlugEtcherLib.PLUG_DATABASE_INITCODE
+                PlugEtcherLib.PLUG_DATABASE_SALT,
+                PlugEtcherLib.PLUG_DATABASE_INITCODE
             );
         }
 
@@ -61,7 +65,8 @@ contract PlugDeployment is Script {
 
         if (PlugEtcherLib.PLUG_REWARDS_ADDRESS.code.length == 0) {
             PlugEtcherLib.FACTORY.safeCreate2(
-                PlugEtcherLib.PLUG_REWARDS_SALT, PlugEtcherLib.PLUG_REWARDS_INITCODE
+                PlugEtcherLib.PLUG_REWARDS_SALT,
+                PlugEtcherLib.PLUG_REWARDS_INITCODE
             );
         }
 
@@ -74,7 +79,8 @@ contract PlugDeployment is Script {
 
         if (PlugEtcherLib.PLUG_TICKET_ADDRESS.code.length == 0) {
             PlugEtcherLib.FACTORY.safeCreate2(
-                PlugEtcherLib.PLUG_TICKET_SALT, PlugEtcherLib.PLUG_TICKET_INITCODE
+                PlugEtcherLib.PLUG_TICKET_SALT,
+                PlugEtcherLib.PLUG_TICKET_INITCODE
             );
         }
 
