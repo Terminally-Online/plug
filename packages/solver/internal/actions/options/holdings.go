5594a0553d5c785a3cfec6e7080f17ffd31b5fc8
Mason <33527785+masonchain@users.noreply.github.com>
2025-04-18 10:04:20 -0500

    fix: add collectibles to balance options

---
+++ packages/solver/internal/actions/options/holdings.go
@@ -0,0 +1,43 @@
+package options
+
+import (
+	"slices"
+	"solver/internal/actions"
+)
+
+/**
+ * GetHoldingsOptions returns a list of options to be used that include held fungibles and collectibles, as well as non-held fungibles.
+ */
+func GetHoldingsOptions[T any](lookup *actions.SchemaLookup[T], index int) ([]actions.Option, error) {
+	holdingsIndex := 1
+	heldFungiblesOptions, err := GetFungiblesHeldOptions(lookup, holdingsIndex)
+	if err != nil {
+		return nil, err
+	}
+
+	otherFungiblesOptions, err := GetFungiblesOptions(lookup, holdingsIndex)
+	if err != nil {
+		return nil, err
+	}
+
+	heldFungiblesMap := make(map[string]actions.Option, len(heldFungiblesOptions))
+	for _, option := range heldFungiblesOptions {
+		heldFungiblesMap[option.Value] = option
+	}
+
+	heldCollectiblesOptions, err := GetCollectiblesHeldOptions(lookup, holdingsIndex)
+	if err != nil {
+		return nil, err
+	}
+
+	// We append held fungibles and collectibles options before including non held fungibles for better display order.
+	holdingsOptions := append(heldFungiblesOptions, heldCollectiblesOptions...)
+
+	allHoldingsOptions := make([]actions.Option, 0, len(holdingsOptions)+len(otherFungiblesOptions))
+	allHoldingsOptions = append(allHoldingsOptions, holdingsOptions...)
+
+	return append(allHoldingsOptions, slices.DeleteFunc(otherFungiblesOptions, func(o actions.Option) bool {
+		_, exists := heldFungiblesMap[o.Value]
+		return exists
+	})...), nil
+}
--- packages/solver/internal/actions/plug/options/transfer.go
+++ packages/solver/internal/actions/plug/options/transfer.go
@@ -1,41 +1,16 @@
 package options
 
 import (
-	"slices"
 	"solver/internal/actions"
 	"solver/internal/actions/options"
 )
 
 func TransferOptions[T any](lookup *actions.SchemaLookup[T]) (map[int]actions.Options, error) {
 	holdingsIndex := 1
-	heldFungiblesOptions, err := options.GetFungiblesHeldOptions(lookup, holdingsIndex)
+	holdingsOptions, err := options.GetHoldingsOptions(lookup, holdingsIndex)
 	if err != nil {
 		return nil, err
 	}
-
-	otherFungiblesOptions, err := options.GetFungiblesOptions(lookup, holdingsIndex)
-	if err != nil {
-		return nil, err
-	}
-
-	heldFungiblesMap := make(map[string]actions.Option, len(heldFungiblesOptions))
-	for _, option := range heldFungiblesOptions {
-		heldFungiblesMap[option.Value] = option
-	}
-
-	// TODO MASON: what search index should I be using?
-	heldNonFungiblesOptions, err := options.GetCollectiblesHeldOptions(lookup, holdingsIndex)
-	if err != nil {
-		return nil, err
-	}
-
-	holdingsOptions := append(heldFungiblesOptions, heldNonFungiblesOptions...)
-	allHoldingsOptions := make([]actions.Option, 0, len(holdingsOptions)+len(otherFungiblesOptions))
-	allHoldingsOptions = append(allHoldingsOptions, holdingsOptions...)
-	allHoldingsOptions = append(allHoldingsOptions, slices.DeleteFunc(otherFungiblesOptions, func(o actions.Option) bool {
-		_, exists := heldFungiblesMap[o.Value]
-		return exists
-	})...)
 
 	recipientIndex := 2
 	recipientOptions, err := options.GetAddressOptions(lookup, recipientIndex)
@@ -44,7 +19,7 @@ func TransferOptions[T any](lookup *actions.SchemaLookup[T]) (map[int]actions.Op
 	}
 
 	return map[int]actions.Options{
-		holdingsIndex:  {Simple: allHoldingsOptions},
+		holdingsIndex:  {Simple: holdingsOptions},
 		recipientIndex: {Simple: recipientOptions},
 	}, nil
 }
