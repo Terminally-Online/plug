f0f3bfb5b9affb9ace585e0193deb902f6091c8b
nftchance <chance@utc24.io>
2025-04-19 23:46:37 -0500

    fix: use AssertFunc in assert integration

--- packages/solver/internal/actions/assert/actions/assert.go
+++ packages/solver/internal/actions/assert/actions/assert.go
@@ -16,26 +16,35 @@ type AssertRequest struct {
 	Assertion string                     `json:"assertion"`
 }
 
+var AssertFunc = actions.ActionOnchainFunctionResponse{
+	Metadata:     plug_assert.PlugAssertMetaData,
+	FunctionName: "assertTrue",
+}
+
 func HandleAssert(lookup *actions.SchemaLookup[AssertRequest]) ([]signature.Plug, error) {
-	assertContract := common.HexToAddress(references.Networks[lookup.ChainId].References["plug"]["assert"])
-	assertAbi, err := plug_assert.PlugAssertMetaData.GetAbi()
+	condition, updates, err := actions.GetAndUpdate(
+		&lookup.Inputs.Condition,
+		lookup.Inputs.Condition.GetValueWithError,
+		&AssertFunc,
+		"y",
+		nil,
+		lookup.PreviousActionDefinition,
+	)
 	if err != nil {
-		return nil, fmt.Errorf("failed to get PlugAssert ABI: %w", err)
+		return nil, fmt.Errorf("failed to get y value: %w", err)
 	}
 
-	functionName := "assertTrue"
-	if lookup.Inputs.Assertion == "false" {
-		functionName = "assertFalse"
-	}
-
-	calldata, err := assertAbi.Pack(functionName, lookup.Inputs.Condition)
+	AssertFunc.FunctionName = lookup.Inputs.Assertion
+	calldata, err := AssertFunc.GetCalldata(condition)
 	if err != nil {
 		return nil, fmt.Errorf("failed to pack assertTrue calldata: %w", err)
 	}
 
+	assertContract := common.HexToAddress(references.Networks[lookup.ChainId].References["plug"]["assert"])
 	return []signature.Plug{{
 		Selector: signature.StaticCall,
 		To:       assertContract,
 		Data:     calldata,
+		Updates:  updates,
 	}}, nil
 }
